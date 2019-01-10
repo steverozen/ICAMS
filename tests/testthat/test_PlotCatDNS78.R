ca54190119a964e13a79fbe9369f8f97465ec188
jnh <44010028+jnh01@users.noreply.github.com>
2019-01-10 17:43:45 +0800

    Create test_PlotCatDNS78.R

---
+++ tests/testthat/test_PlotCatDNS78.R
@@ -0,0 +1,9 @@
+context("PlotCatDNS78")
+
+test_that("PlotCatDNS78 function is working properly", {
+  par(mar = c(6, 4, 6, 2))
+  catalog <- ReadCatDNS78("testdata/regress.cat.dns.78.csv")
+  out <- PlotCatDNS78(catalog[, 1, drop = FALSE], id = "test",
+                      type = "density", abundance = .abundance.2bp)
+  expect_equal(out, TRUE)
+})
