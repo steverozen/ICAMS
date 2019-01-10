5588c8d4bc3b8cf8a7ddbea5877fdb0e1825ee67
jnh <44010028+jnh01@users.noreply.github.com>
2019-01-10 17:41:02 +0800

    Create test_Cat192ToPdf.R

---
+++ tests/testthat/test_Cat192ToPdf.R
@@ -0,0 +1,14 @@
+context("Cat192ToPdf")
+
+test_that("Cat192ToPdf function is working properly", {
+  catalog <- ReadCat192("testdata/regress.cat.192.csv")
+  colnames(catalog) <- paste0("HepG2_", 1 : 4)
+  cat1 <- catalog[, 1, drop = FALSE]
+  cat2 <- catalog[, 2, drop = FALSE]
+  cat3 <- catalog[, 3, drop = FALSE]
+  cat4 <- catalog[, 4, drop = FALSE]
+  cat <- cbind(cat1, cat1, cat2, cat2, cat3, cat3, cat4, cat4)
+  type <- c("counts", "signature")
+  out <- Cat192ToPdf(cat, "PlotCat192.test.pdf", type = rep(type, 4))
+  expect_equal(out, TRUE)
+})
