a687ca999de4516296d5234f7b25692eb4f23727
jnh <44010028+jnh01@users.noreply.github.com>
2019-01-10 17:43:14 +0800

    Create test_PlotCat96.R

---
+++ tests/testthat/test_PlotCat96.R
@@ -0,0 +1,9 @@
+context("PlotCat96")
+
+test_that("PlotCat96 function is working properly", {
+  par(mar = c(11.5, 5.1, 4.6, 0))
+  catalog <- ReadCat96("testdata/regress.cat.96.csv")
+  out <- PlotCat96(catalog[, 1, drop = FALSE], id = "test",
+            type = "density", abundance = .abundance.3bp)
+  expect_equal(out, TRUE)
+})
