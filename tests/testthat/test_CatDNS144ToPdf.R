7ebe56748b3855aa8440109da58acd1e28dcdf9d
jnh <44010028+jnh01@users.noreply.github.com>
2019-01-10 17:41:37 +0800

    Create test_CatDNS144ToPdf.R

---
+++ tests/testthat/test_CatDNS144ToPdf.R
@@ -0,0 +1,15 @@
+context("CatDNS144ToPdf")
+
+test_that("CatDNS144ToPdf function is working properly", {
+  catalog <- ReadCatDNS144("testdata/regress.cat.dns.144.csv")
+  colnames(catalog) <- paste0("HepG2_", 1 : 4)
+  cat1 <- catalog[, 1, drop = FALSE]
+  cat2 <- catalog[, 2, drop = FALSE]
+  cat3 <- catalog[, 3, drop = FALSE]
+  cat4 <- catalog[, 4, drop = FALSE]
+  cat <- cbind(cat1, cat1, cat1, cat2, cat2, cat2, cat3, cat3, cat3,
+               cat4, cat4, cat4)
+  type <- rep(c("counts", "signature"), each = 3)
+  out <- CatDNS144ToPdf(cat, "PlotCatDNS144.test.pdf", type = rep(type, 2))
+  expect_equal(out, TRUE)
+})
