7a04d0d2b5cbf311ffc2c8ab11531d338a5703ca
jnh <44010028+jnh01@users.noreply.github.com>
2019-01-14 17:57:39 +0800

    Update test_PlotCat96.R

--- tests/testthat/test_PlotCat96.R
+++ tests/testthat/test_PlotCat96.R
@@ -1,7 +1,7 @@
 context("PlotCat96")
 
 test_that("PlotCat96 function is working properly", {
-  par(mar = c(11.5, 5.1, 4.6, 0))
+  par(mar = c(11.5, 5.1, 4.6, 3))
   catalog <- ReadCat96("testdata/regress.cat.96.csv")
   out <- PlotCat96(catalog[, 1, drop = FALSE], id = "test",
             type = "density", abundance = .abundance.3bp)
