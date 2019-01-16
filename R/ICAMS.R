f45c659f069a3ad3cff1c61fa4a8ec0cc1f52124
steverozen <steve@steverozen.net>
2019-01-16 16:01:09 +0800

    added man page for entire package

---
+++ R/ICAMS.R
@@ -0,0 +1,17 @@
+#' ICAMS: In Depth Characterization and Analysis of Mutational Signatures
+#'
+#' This package provides ...
+#'
+#' @section Plotting Signatures:
+#' Functions for comparing mutational signatures and
+#' sets of mutational signatures:
+#' \code{\link{PlotCat96}}
+#'
+#' @section Generating mutational signature catalogs from VCF files:
+#' fwriteDataFrame
+#'
+#'
+#' @docType package
+#' @name ICAMS
+#' @keywords internal
+NULL
