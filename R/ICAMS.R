a6df4ce29a3793f6b230253f55b0dbb0656bf5fa
jnh <44010028+jnh01@users.noreply.github.com>
2019-01-23 14:22:18 +0800

    Update ICAMS.R

--- R/ICAMS.R
+++ R/ICAMS.R
@@ -3,13 +3,6 @@
 #' This package has functions to read in VCF files from Strelka and GATK,
 #' create SNS, DNS, ID catalogs and do different types of plotting.
 #'
-#' @section Plotting catalogs:
-#' Functions for plotting the mutation catalog of one sample:
-#' \code{\link{PlotCatalog}}
-#'
-#' Functions for plotting mutation catalog of different samples to a PDF file:
-#' \code{\link{CatalogToPdf}}
-#'
 #' @section Reading catalogs:
 #' Functions for reading a catalog in PCAWG7 format from path:
 #' \code{\link{ReadCatalog}}
@@ -17,6 +10,17 @@
 #' @section Writing catalogs:
 #' Functions for writting a mutation catalog to a file on disk:
 #' \code{\link{WriteCatalog}}
+#'
+#' @section Collapsing catalogs:
+#' Functions for collapsing a mutation catalog to a canonical one:
+#' \code{\link{CollapseCatalog}}
+#'
+#' @section Plotting catalogs:
+#' Functions for plotting the mutation catalog of one sample:
+#' \code{\link{PlotCatalog}}
+#'
+#' Functions for plotting mutation catalog of different samples to a PDF file:
+#' \code{\link{CatalogToPdf}}
 #'
 #' @docType package
 #' @name ICAMS
