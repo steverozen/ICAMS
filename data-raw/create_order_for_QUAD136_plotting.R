fe4bcd76489d691e1a5bd1fde9191bf43b409272
jnh <44010028+jnh01@users.noreply.github.com>
2019-01-25 18:18:20 +0800

    Create create_order_for_QUAD136_plotting.R

---
+++ data-raw/create_order_for_QUAD136_plotting.R
@@ -0,0 +1,16 @@
+CreateOrderForQUAD136Plotting <- function() {
+  ref.order <- c("AC", "AT", "GC", "CC", "CG", "CT", "TA", "TC", "TG", "TT")
+  base <- c("A", "C", "G", "T")
+  rev.base <- c("T", "G", "C", "A")
+  output <- character(0)
+  for (i1 in ref.order) {
+    for (i2 in rev.base) {
+      for (i3 in base) {
+        output <- c(output, paste0(i2, i1, i3))
+      }
+    }
+  }
+  return(output)
+}
+
+.order.for.QUAD136.plotting <<- CreateOrderForQUAD136Plotting()
