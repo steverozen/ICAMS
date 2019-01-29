80f4de8d313ff028fc851c1c518d9e03e33c3341
jnh <44010028+jnh01@users.noreply.github.com>
2019-01-29 17:52:28 +0800

    Update test_PlotCatQUAD136.R

--- tests/testthat/test_PlotCatQUAD136.R
+++ tests/testthat/test_PlotCatQUAD136.R
@@ -1,6 +1,7 @@
 context("PlotCatQUAD136")
 
 test_that("PlotCatQUAD136 function is working properly", {
+  par(oma = c(2, 2, 2, 0))
   catalog <- ReadCatQUAD136("testdata/regress.cat.quad.136.csv")
   out <- PlotCatQUAD136(catalog[, 1, drop = FALSE], id = "test",
                       type = "density", abundance = .abundance.4bp)
