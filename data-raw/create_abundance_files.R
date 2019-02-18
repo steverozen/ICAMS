2d286b7e26c19213ed01e6c46f330a9bd924e08e
jnh <44010028+jnh01@users.noreply.github.com>
2019-02-18 17:29:15 +0800

    Changed the names of the abundance files

--- data-raw/create_abundance_files.R
+++ data-raw/create_abundance_files.R
@@ -1,4 +1,4 @@
-abundance.2bp <- CreateDinucAbundance("data-raw/hs37d5_masked_4bp.txt")
-abundance.3bp <- CreateTrinucAbundance("data-raw/hs37d5_masked_3bp.txt")
-abundance.4bp <- CreateTetranucAbundance("data-raw/hs37d5_masked_4bp.txt")
-abundance.5bp <- CreatePentanucAbundance("data-raw/hs37d5_masked_5bp.txt")
+abundance.2bp.GRCh37 <- CreateDinucAbundance("data-raw/hs37d5_masked_4bp.txt")
+abundance.3bp.GRCh37 <- CreateTrinucAbundance("data-raw/hs37d5_masked_3bp.txt")
+abundance.4bp.GRCh37 <- CreateTetranucAbundance("data-raw/hs37d5_masked_4bp.txt")
+abundance.5bp.GRCh37 <- CreatePentanucAbundance("data-raw/hs37d5_masked_5bp.txt")
