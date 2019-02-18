40a46628576db8177cc14974893f5cc6c255d1c1
jnh <44010028+jnh01@users.noreply.github.com>
2019-02-18 18:27:20 +0800

    Many updates to the testthat folder

--- tests/testthat/test_PlotCatDNS136.R
+++ tests/testthat/test_PlotCatDNS136.R
@@ -1,6 +1,6 @@
-context("PlotCatQUAD136")
+context("PlotCatDNS136")
 
-test_that("PlotCatQUAD136 function is working properly", {
+test_that("PlotCatDNS136 function is working properly", {
   par(oma = c(2, 2, 2, 0))
   catalog <- ReadCatDNS136("testdata/regress.cat.dns.136.csv")
   out <- PlotCatDNS136(catalog[, 1, drop = FALSE], id = "test",
