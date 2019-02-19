85f37feeab6c3ba5535c39695ee6ac777f5e1905
jnh <44010028+jnh01@users.noreply.github.com>
2019-02-19 14:59:52 +0800

    Many updates

--- data-raw/create_abundance_files.R
+++ data-raw/create_abundance_files.R
@@ -1,4 +1,9 @@
-abundance.2bp.GRCh37 <- CreateDinucAbundance("data-raw/hs37d5_masked_4bp.txt")
-abundance.3bp.GRCh37 <- CreateTrinucAbundance("data-raw/hs37d5_masked_3bp.txt")
-abundance.4bp.GRCh37 <- CreateTetranucAbundance("data-raw/hs37d5_masked_4bp.txt")
-abundance.5bp.GRCh37 <- CreatePentanucAbundance("data-raw/hs37d5_masked_5bp.txt")
+abundance.2bp.genome.GRCh37 <- CreateDinucAbundance("data-raw/hs37d5_masked_4bp.txt")
+abundance.3bp.genome.GRCh37 <- CreateTrinucAbundance("data-raw/hs37d5_masked_3bp.txt")
+abundance.4bp.genome.GRCh37 <- CreateTetranucAbundance("data-raw/hs37d5_masked_4bp.txt")
+abundance.5bp.genome.GRCh37 <- CreatePentanucAbundance("data-raw/hs37d5_masked_5bp.txt")
+
+abundance.2bp.exome.GRCh37 <- CreateDinucAbundance("data-raw/hs37d5_masked_AgilentV6_4bp.txt")
+abundance.3bp.exome.GRCh37 <- CreateTrinucAbundance("data-raw/hs37d5_masked_AgilentV6_3bp.txt")
+abundance.4bp.exome.GRCh37 <- CreateTetranucAbundance("data-raw/hs37d5_masked_AgilentV6_4bp.txt")
+abundance.5bp.exome.GRCh37 <- CreatePentanucAbundance("data-raw/hs37d5_masked_AgilentV6_5bp.txt")
