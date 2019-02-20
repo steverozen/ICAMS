39367bb56fc1d19d15436db3f0e69dd3b0450224
jnh <44010028+jnh01@users.noreply.github.com>
2019-02-20 18:05:15 +0800

    Update ICAMS.R

--- R/ICAMS.R
+++ R/ICAMS.R
@@ -9,17 +9,28 @@
 #' between GRCh37 and GRCh38).
 #'
 #' @section Reading VCF files:
-#' \code{\link{ReadListOfStrelkaVCFs}}, which only reads Strelka single nucleotide
+#' \code{\link{ReadListOfStrelkaSNSVCFs}}, which only reads Strelka single nucleotide
 #' substitution (SNS) VCFs, not Strelka
-#' indel VCFS.  Handling of indel VCFs for Strelka is not
-#' finshed yet.
+#' indel VCFS.\cr
+#' \code{\link{ReadListOfStrelkaIDVCFs}}, which only reads Strelka indels (ID) VCFs, not Strelka
+#' SNS VCFS.\cr
 #' \code{\link{ReadListOfMutectVCFs}}, which reads Mutect VCFs, which contain indels and double
 #' nucleotide substitutions (DNSs) as well and SNSs.
 #'
 #' @section Splitting of in-memory VCFs:
-#' \code{\link{SplitListOfStrelkaVCFs}}, which splits Strelka SNS VCFs
-#' into SNS and inferred DNS VCFs, and \code{\link{SplitListOfMutectVCFs}},
-#' which separates Mutect VCFs into their SNS, DNS, and indel components.
+#' \code{\link{SplitListOfStrelkaSNSVCFs}}, which splits Strelka SNS VCFs
+#' into SNS and inferred DNS VCFs. \cr
+#'  \code{\link{SplitListOfMutectVCFs}}, which separates Mutect VCFs into their
+#'  SNS, DNS, and indel components.
+#'
+#' @section Creating catalogs from VCF files:
+#' \code{\link{StrelkaSNSVCFFilesToCatalog}}, which creates 3 SNS catalogs (96,
+#' 192, 1536) and 3 DNS catalogs (78, 136, 144) from the Strelka SNS VCFs.\cr
+#' \code{\link{StrelkaIDVCFFilesToCatalog}}, which creates ID (indels) catalog
+#' from the Strelka ID VCFs.\cr
+#' \code{\link{MutectVCFFilesToCatalog}}, which creates 3 SNS catalogs (96,
+#' 192, 1536), 3 DNS catalogs (78, 136, 144) and ID (indels) catalog from the
+#' Mutect VCFs.
 #'
 #' @section Reading catalogs:
 #' Functions for reading a catalog in PCAWG7 format from path:
@@ -34,11 +45,8 @@
 #' \code{\link{CollapseCatalog}}
 #'
 #' @section Plotting catalogs:
-#' Functions for plotting the mutation catalog of one sample:
-#' \code{\link{PlotCatalog}}
-#'
-#' Functions for plotting mutation catalog of different samples to a PDF file:
-#' \code{\link{CatalogToPdf}}
+#' Functions for plotting mutation catalog of various samples to a PDF file:
+#' \code{\link{PlotCatalogToPdf}}
 #'
 #' @docType package
 #' @name ICAMS
