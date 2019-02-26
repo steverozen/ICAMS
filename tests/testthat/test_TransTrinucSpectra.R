bbeca932b57d1588a293ef4cbae6c3007048a645
jnh <44010028+jnh01@users.noreply.github.com>
2019-02-26 11:58:14 +0800

    Updates to the testthat folder

--- tests/testthat/test_TransTrinucSpectra.R
+++ tests/testthat/test_TransTrinucSpectra.R
@@ -3,8 +3,9 @@ context("TransTrinucSpectra")
 test_that("TransTrinucSpectra function is working properly", {
   human.catSNS96 <- ReadCatSNS96("testdata/regress.cat.sns.96.csv")
   mouse.catSNS96 <- ReadCatSNS96("testdata/mouse.cat.sns.96.csv")
-  inferred.mouse.catSNS96 <- TransTrinucSpectra(human.catSNS96,
-                                                source.abundance = "GRCh37.genome",
-                                                target.abundance = "GRCm38.genome")
+  inferred.mouse.catSNS96 <-
+    TransTrinucSpectra(human.catSNS96,
+                       source.abundance = "GRCh37.genome",
+                       target.abundance = "GRCm38.genome")
   expect_equal(inferred.mouse.catSNS96, mouse.catSNS96)
 })
