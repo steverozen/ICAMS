f86a0a22962c1237caa549b01d988cbc8e22d10b
jnh <44010028+jnh01@users.noreply.github.com>
2019-02-28 12:37:26 +0800

    Create test_AddAndCheckSequenceID.R

---
+++ tests/testthat/test_AddAndCheckSequenceID.R
@@ -0,0 +1,22 @@
+context("AddAndCheckSequenceID")
+
+test_that("AddAndCheckSequenceID function is working properly", {
+  id.vcf <- ReadStrelkaIDVCF("testdata/Strelka.ID.GRCh37.vcf")
+  strelka.ID.vcf.GRCh37 <-
+    AddAndCheckSequenceID(id.vcf, genome = BSgenome.Hsapiens.1000genomes.hs37d5)
+  df <- AddAndCheckSequenceID(id.vcf, genome = BSgenome.Hsapiens.1000genomes.hs37d5)
+  df1 <- AddAndCheckSequenceID(id.vcf, genome = "GRCh37")
+  df2 <- AddAndCheckSequenceID(id.vcf, genome = "hg19")
+  expect_equal(df, df1)
+  expect_equal(df, df2)
+
+  id.vcf <- ReadStrelkaIDVCF("testdata/Strelka.ID.GRCh38.vcf")
+  strelka.ID.vcf.GRCh38 <-
+    AddAndCheckSequenceID(id.vcf, genome = BSgenome.Hsapiens.UCSC.hg38)
+  df3 <- AddAndCheckSequenceID(id.vcf, genome = BSgenome.Hsapiens.1000genomes.hs37d5)
+  df4 <- AddAndCheckSequenceID(id.vcf, genome = "GRCh38")
+  df5 <- AddAndCheckSequenceID(id.vcf, genome = "hg38")
+  expect_equal(df3, df4)
+  expect_equal(df3, df5)
+
+})
