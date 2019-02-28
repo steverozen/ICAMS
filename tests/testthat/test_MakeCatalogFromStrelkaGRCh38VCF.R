6e1e19ddbcd807c6fdd850e8f7d6be655d987508
jnh <44010028+jnh01@users.noreply.github.com>
2019-02-28 18:01:14 +0800

    Create test_MakeCatalogFromStrelkaGRCh38VCF.R

---
+++ tests/testthat/test_MakeCatalogFromStrelkaGRCh38VCF.R
@@ -0,0 +1,27 @@
+context("MakeCatalogFromStrelkaGRCh38VCF")
+
+test_that("Function are working properly to make catalogs from Strelka GRCh38 VCF", {
+
+  cat1 <- StrelkaSNSVCFFilesToCatalog("testdata/Strelka.SNS.GRCh38.vcf",
+                                      genome = BSgenome.Hsapiens.UCSC.hg38,
+                                      trans.ranges = trans.ranges.GRCh38)
+  cat2 <- StrelkaSNSVCFFilesToCatalog("testdata/Strelka.SNS.GRCh38.vcf",
+                                      genome = "GRCh38",
+                                      trans.ranges = trans.ranges.GRCh38)
+  cat3 <- StrelkaSNSVCFFilesToCatalog("testdata/Strelka.SNS.GRCh38.vcf",
+                                      genome = "hg38",
+                                      trans.ranges = trans.ranges.GRCh38)
+  expect_equal(cat1, cat2)
+  expect_equal(cat1, cat3)
+
+
+  cat4 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
+                                       genome = BSgenome.Hsapiens.UCSC.hg38)
+  cat5 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
+                                     genome = "GRCh38")
+  cat6 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
+                                     genome = "hg38")
+  expect_equal(cat4, cat5)
+  expect_equal(cat4, cat6)
+
+})
