10b1be8795c28a7a4842f009474ef30ca459c903
jnh <44010028+jnh01@users.noreply.github.com>
2019-02-28 12:55:36 +0800

    Created exported data trans.ranges.GRCh38

---
+++ data-raw/create_testdata_for_AddSequence_and_AddAndCheckSequenceID.R
@@ -0,0 +1,22 @@
+list.of.vcf <- ReadAndSplitStrelkaSNSVCFs("data-raw/Strelka.SNS.GRCh37.vcf")
+sns.vcf <- list.of.vcf$SNS.vcfs[[1]]
+strelka.SNS.vcf.GRCh37 <-
+  AddSequence(sns.vcf, genome = BSgenome.Hsapiens.1000genomes.hs37d5)
+
+list.of.vcf <- ReadAndSplitStrelkaSNSVCFs("data-raw/Strelka.SNS.GRCh38.vcf")
+sns.vcf <- list.of.vcf$SNS.vcfs[[1]]
+strelka.SNS.vcf.GRCh38 <-
+  AddSequence(sns.vcf, genome = BSgenome.Hsapiens.UCSC.hg38)
+
+save(strelka.SNS.vcf.GRCh37, strelka.SNS.vcf.GRCh38, file = "test_AddSequence.Rdata")
+
+id.vcf <- ReadStrelkaIDVCF("data-raw/Strelka.ID.GRCh37.vcf")
+strelka.ID.vcf.GRCh37 <-
+  AddAndCheckSequenceID(id.vcf, genome = BSgenome.Hsapiens.1000genomes.hs37d5)
+
+id.vcf <- ReadStrelkaIDVCF("data-raw/Strelka.ID.GRCh38.vcf")
+strelka.ID.vcf.GRCh38 <-
+  AddAndCheckSequenceID(id.vcf, genome = BSgenome.Hsapiens.UCSC.hg38)
+
+save(strelka.ID.vcf.GRCh37, strelka.ID.vcf.GRCh38,
+     file = "test_AddAndCheckSequenceID.Rdata")
--- data-raw/create_trans_ranges.R
+++ data-raw/create_trans_ranges.R
@@ -1,3 +1,6 @@
+trans.ranges.GRCh38 <-
+  ReadTranscriptRanges("data-raw/TranscriptRanges.GRCh38.tsv")
+
 trans.ranges.GRCh37 <-
   ReadTranscriptRanges("data-raw/TranscriptRanges.GRCh37.tsv")
 
