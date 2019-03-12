65128fd4eac85edc5661506d4a1b567490710aa6
jnh <44010028+jnh01@users.noreply.github.com>
2019-03-12 11:11:11 +0800

    Added more tests for function TransformCatalog

--- tests/testthat/test_TransformCatalog.R
+++ tests/testthat/test_TransformCatalog.R
@@ -24,7 +24,7 @@ test_that("TransformCatalog genome counts -> exome signature,
 
 
 test_that("TransformCatalog genome-count signature -> denisty signature,
-          and exome counts -> genome counts", {
+          and genome counts -> exome-count signature", {
             cat <- ReadCatSNS96("testdata/regress.cat.sns.96.csv")
 
             genome.count.signature <-
@@ -52,6 +52,23 @@ test_that("TransformCatalog genome-count signature -> denisty signature,
                                target.type = "signature",
                                which.n = 3)
 
+            exome.count.signature <-
+              TransformCatalog(cat, source.abundance = "GRCh37.genome",
+                               target.abundance = "GRCh37.exome",
+                               source.type = "counts",
+                               target.type = "signature", which.n = 3)
+
+            exome.counts <-
+              TransformCatalog(cat, source.abundance = "GRCh37.genome",
+                               target.abundance = "GRCh37.exome",
+                               source.type = "counts",
+                               target.type = "counts", which.n = 3)
+
+            exome.count.signature2 <-
+              TransformCatalog(exome.counts, source.abundance = "GRCh37.exome",
+                               target.abundance = "GRCh37.exome",
+                               source.type = "counts",
+                               target.type = "signature", which.n = 3)
 
             expect_equal(density.signature2, density.signature)
 
@@ -96,6 +113,67 @@ test_that("TransformCatalog genome counts -> density,
             expect_equal(x1, x3)
 
           })
+
+test_that("TransformCatalog genome GRCh37 counts -> genome GRCh37 density,
+           and genome GRCh37 counts -> genome GRCh38 count -> genome GRCh38 density", {
+             cat <- ReadCatSNS96("testdata/regress.cat.sns.96.csv")
+
+             x1 <- TransformCatalog(cat, source.abundance = "GRCh37.genome",
+                                    target.abundance = NULL,
+                                    source.type = "counts",
+                                    target.type = "density", which.n = 3)
+
+             x2 <- TransformCatalog(cat, source.abundance = "GRCh37.genome",
+                                    target.abundance = "GRCh38.genome",
+                                    source.type = "counts",
+                                    target.type = "counts", which.n = 3)
+
+             x3 <- TransformCatalog(x1, source.abundance = "GRCh38.genome",
+                                    target.abundance = NULL,
+                                    source.type = "counts",
+                                    target.type = "density", which.n = 3)
+
+             expect_equal(x1, x3)
+
+           })
+
+test_that("TransformCatalog genome GRCh37 counts -> genome GRCh38 counts,
+           and genome GRCh37 counts -> genome GRCh37 density -> genome GRCh38 counts", {
+             cat <- ReadCatSNS96("testdata/regress.cat.sns.96.csv")
+
+             x1 <- TransformCatalog(cat, source.abundance = "GRCh37.genome",
+                                    target.abundance = "GRCh38.genome",
+                                    source.type = "counts",
+                                    target.type = "counts", which.n = 3)
+
+             x2 <- TransformCatalog(cat, source.abundance = "GRCh37.genome",
+                                    target.abundance = NULL,
+                                    source.type = "counts",
+                                    target.type = "density", which.n = 3)
+
+             x3 <- TransformCatalog(x2, source.abundance = NULL,
+                                    target.abundance = "GRCh38.genome",
+                                    source.type = "density",
+                                    target.type = "counts", which.n = 3)
+
+             expect_gt(colSums(x1[, 1, drop = FALSE]), colSums(x3[, 1, drop = FALSE]))
+
+           })
+
+test_that("TransformCatalog function: going from density to genome counts", {
+  cat <- ReadCatSNS96("testdata/regress.cat.sns.96.csv")
+
+  x1 <- TransformCatalog(cat, source.abundance = "GRCh37.genome",
+                         source.type = "counts",
+                         target.type = "density", which.n = 3)
+
+  x2 <- TransformCatalog(x1, source.abundance = NULL,
+                         source.type = "density",
+                         target.abundance = "GRCh37.genome",
+                         target.type = "counts", which.n = 3)
+
+  expect_equal(cat, x2)
+})
 
 test_that("TransformCatalog function: going from density to density,
           error message expected", {
