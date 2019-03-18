c5ab553596891bf4f955c24d79da449566bb935c
jnh <44010028+jnh01@users.noreply.github.com>
2019-03-18 10:34:34 +0800

    Update create_manual.R

--- data-raw/create_manual.R
+++ data-raw/create_manual.R
@@ -1,1 +1,3 @@
-system("R CMD Rd2pdf . --title=ICAMS --output=./ICAMS.manual.pdf --force --no-clean --internals")
+devtools::build_manual()
+
+#system("R CMD Rd2pdf . --title=ICAMS --output=./ICAMS.manual.pdf --force --no-clean --internals")
