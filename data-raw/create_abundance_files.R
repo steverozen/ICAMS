0fff22c29de6bf756947babe3a473b9353ff1fb4
jnh <44010028+jnh01@users.noreply.github.com>
2019-03-26 11:40:50 +0800

    Created flat abundance

--- data-raw/create_abundance_files.R
+++ data-raw/create_abundance_files.R
@@ -37,3 +37,15 @@ abundance.4bp.exome.GRCh38 <-
   CreateTetranucAbundance("data-raw/nucleotide_abundance/hg38_masked_AgilentV6_4bp.txt")
 abundance.5bp.exome.GRCh38 <-
   CreatePentanucAbundance("data-raw/nucleotide_abundance/hg38_masked_AgilentV6_5bp.txt")
+
+abundance.2bp.flat <- rep(1, length(abundance.2bp.exome.GRCh37))
+names(abundance.2bp.flat) <- names(abundance.2bp.exome.GRCh37)
+
+abundance.3bp.flat <- rep(1, length(abundance.3bp.exome.GRCh37))
+names(abundance.3bp.flat) <- names(abundance.3bp.exome.GRCh37)
+
+abundance.4bp.flat <- rep(1, length(abundance.4bp.exome.GRCh37))
+names(abundance.4bp.flat) <- names(abundance.4bp.exome.GRCh37)
+
+abundance.5bp.flat <- rep(1, length(abundance.5bp.exome.GRCh37))
+names(abundance.5bp.flat) <- names(abundance.5bp.exome.GRCh37)
