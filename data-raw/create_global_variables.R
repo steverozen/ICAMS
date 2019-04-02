634f48a94b77a049d4a8a2529ecf49373a15d15f
jnh <44010028+jnh01@users.noreply.github.com>
2019-04-02 10:07:43 +0800

    Update create_global_variables.R

--- data-raw/create_global_variables.R
+++ data-raw/create_global_variables.R
@@ -1,6 +1,7 @@
 usethis::use_data(catalog.row.order,
                   trans.ranges.GRCh37,
                   trans.ranges.GRCh38,
+                  trans.ranges.GRCm38,
                   overwrite = TRUE)
 
 usethis::use_data(to.reorder.SNS.192.for.plotting,
