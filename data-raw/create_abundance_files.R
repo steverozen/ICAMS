4a800c856d8036d556df1dad686b8a1f7df4367f
jnh <44010028+jnh01@users.noreply.github.com>
2019-04-04 09:02:49 +0800

    Update create_abundance_files.R

--- data-raw/create_abundance_files.R
+++ data-raw/create_abundance_files.R
@@ -33,7 +33,7 @@ abundance.5bp.exome.GRCh37 <-
 
 #######################################################################################
 abundance.2bp.genome.GRCh38 <-
-  CreateDinucAbundance("data-raw/new_abundance/GRCh38_masked_4bp.csv")
+  CreateDinucAbundance("data-raw/new_abundance/GRCh38_masked_2bp.csv")
 abundance.3bp.genome.GRCh38 <-
   CreateTrinucAbundance("data-raw/new_abundance/GRCh38_masked_3bp.csv")
 abundance.4bp.genome.GRCh38 <-
@@ -61,14 +61,14 @@ abundance.5bp.exome.GRCh38 <-
 
 #########################################################################################
 
-abundance.2bp.flat <- rep(1, length(abundance.2bp.exome.GRCh37))
-names(abundance.2bp.flat) <- names(abundance.2bp.exome.GRCh37)
+abundance.2bp.flat <- rep(1, length(abundance.2bp.genome.GRCh37))
+names(abundance.2bp.flat) <- names(abundance.2bp.genome.GRCh37)
 
-abundance.3bp.flat <- rep(1, length(abundance.3bp.exome.GRCh37))
-names(abundance.3bp.flat) <- names(abundance.3bp.exome.GRCh37)
+abundance.3bp.flat <- rep(1, length(abundance.3bp.genome.GRCh37))
+names(abundance.3bp.flat) <- names(abundance.3bp.genome.GRCh37)
 
-abundance.4bp.flat <- rep(1, length(abundance.4bp.exome.GRCh37))
-names(abundance.4bp.flat) <- names(abundance.4bp.exome.GRCh37)
+abundance.4bp.flat <- rep(1, length(abundance.4bp.genome.GRCh37))
+names(abundance.4bp.flat) <- names(abundance.4bp.genome.GRCh37)
 
-abundance.5bp.flat <- rep(1, length(abundance.5bp.exome.GRCh37))
-names(abundance.5bp.flat) <- names(abundance.5bp.exome.GRCh37)
+abundance.5bp.flat <- rep(1, length(abundance.5bp.genome.GRCh37))
+names(abundance.5bp.flat) <- names(abundance.5bp.genome.GRCh37)
