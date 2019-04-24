c11e8c53a13caa79ef1c572180cbd02a58967a0b
jnh <44010028+jnh01@users.noreply.github.com>
2019-04-24 19:03:06 +0800

    Update create_ranges.R

--- data-raw/create_ranges.R
+++ data-raw/create_ranges.R
@@ -2,15 +2,17 @@
 
 cat(getwd(), "\n")
 
+trans.ranges.GRCh37 <-
+  CreateTransRanges("data-raw/ranges/gencode.GRCh37.annotation.gtf.gz")
+
 trans.ranges.GRCh38 <-
   CreateTransRanges("data-raw/ranges/gencode.GRCh38.p12.annotation.gff3.gz")
 
-trans.ranges.GRCh37 <-
-  CreateTransRanges("data-raw/ranges/gencode.GRCh37.annotation.gtf.gz")
+exome.ranges.GRCh37 <-
+  ReadBedRanges("data-raw/ranges/SureSelect_Human_All_Exon_V6.hg19.bed")
 
 exome.ranges.GRCh38 <-
   ReadBedRanges("data-raw/ranges/SureSelect_Human_All_Exon_V6.GRCh38.bed")
 
-exome.ranges.GRCh37 <-
-  ReadBedRanges("data-raw/ranges/SureSelect_Human_All_Exon_V6.hg19.bed")
+
 
