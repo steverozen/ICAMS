29580459e3a97ba6488996b54ae0ec9614729317
jnh <44010028+jnh01@users.noreply.github.com>
2019-05-02 18:59:22 +0800

    Update test_PlotCatalogToPdf.SNSClassStrandBias.R

--- tests/testthat/test_PlotCatalogToPdf.SNSClassStrandBias.R
+++ tests/testthat/test_PlotCatalogToPdf.SNSClassStrandBias.R
@@ -1,13 +1,49 @@
 context("PlotCatalogToPdf.SNSClassStrandBias")
 
 test_that("PlotCatalogToPdf.SNSClassStrandBias function is working properly", {
-  catalog <- ReadCatalog("testdata/regress.cat.sns.192.csv", ref.genome = "GRCh37",
-                         region = "genome", catalog.type = "counts")
-  colnames(catalog) <- paste0("HepG2_", 1 : 4)
+  catalog.counts <- ReadCatalog("testdata/regress.cat.sns.192.csv",
+                                ref.genome = "GRCh37",
+                                region = "genome", catalog.type = "counts")
+  colnames(catalog.counts) <- paste0("HepG2_", 1 : 4)
   out <-
-    PlotCatalogToPdf(catalog, filename = "PlotSNSClassStrandBias.test.pdf",
+    PlotCatalogToPdf(catalog.counts,
+                     filename = "PlotSNSClassStrandBias.counts.test.pdf",
                      strandbias = TRUE)
   expect_equal(out, TRUE)
-  unlink("PlotSNSClassStrandBias.test.pdf")
+
+  catalog.density <-
+    TransformCatalog(catalog.counts, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density")
+  out <-
+    PlotCatalogToPdf(catalog.density,
+                     filename = "PlotSNSClassStrandBias.density.test.pdf",
+                     strandbias = TRUE)
+  expect_equal(out, TRUE)
+
+  catalog.counts.signature <-
+    TransformCatalog(catalog.counts, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "counts.signature")
+  out <-
+    PlotCatalogToPdf(catalog.counts.signature,
+                     filename = "PlotSNSClassStrandBias.counts.signature.test.pdf",
+                     strandbias = TRUE)
+  expect_equal(out, TRUE)
+
+  catalog.density.signature <-
+    TransformCatalog(catalog.counts, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density.signature")
+  out <-
+    PlotCatalogToPdf(catalog.density.signature,
+                     filename = "PlotSNSClassStrandBias.density.signature.test.pdf",
+                     strandbias = TRUE)
+  expect_equal(out, TRUE)
+
+  unlink("PlotSNSClassStrandBias.counts.test.pdf")
+  unlink("PlotSNSClassStrandBias.density.test.pdf")
+  unlink("PlotSNSClassStrandBias.counts.signature.test.pdf")
+  unlink("PlotSNSClassStrandBias.density.signature.test.pdf")
   graphics.off()
 })
