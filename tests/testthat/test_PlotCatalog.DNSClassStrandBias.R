d3b37083c11e943dd0907c3854b6e5a214c7bbde
jnh <44010028+jnh01@users.noreply.github.com>
2019-05-02 18:17:37 +0800

    Update test_PlotCatalog.DNSClassStrandBias.R

--- tests/testthat/test_PlotCatalog.DNSClassStrandBias.R
+++ tests/testthat/test_PlotCatalog.DNSClassStrandBias.R
@@ -4,7 +4,7 @@ test_that("PlotCatalog.DNSClassStrandBias function is working properly", {
   par(mar = c(5, 8, 5, 1))
   catalog.counts <- ReadCatalog("testdata/regress.cat.dns.144.csv",
                                 ref.genome = "GRCh37",
-                                region = "transcript", catalog.type = "counts")
+                                region = "genome", catalog.type = "counts")
   cat.counts <- catalog.counts[, 1, drop = FALSE]
   cat.counts <- PreserveCatalogAttribute(catalog.counts, cat.counts)
   out <- PlotCatalog(cat.counts)
@@ -12,21 +12,21 @@ test_that("PlotCatalog.DNSClassStrandBias function is working properly", {
 
   cat.density <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
-                     target.region = "transcript",
+                     target.region = "genome",
                      target.catalog.type = "density")
   out <- PlotCatalog(cat.density)
   expect_equal(out, TRUE)
 
   cat.counts.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
-                     target.region = "transcript",
+                     target.region = "genome",
                      target.catalog.type = "counts.signature")
   out <- PlotCatalog(cat.counts.signature)
   expect_equal(out, TRUE)
 
   cat.density.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
-                     target.region = "transcript",
+                     target.region = "genome",
                      target.catalog.type = "density.signature")
   out <- PlotCatalog(cat.density.signature)
   expect_equal(out, TRUE)
