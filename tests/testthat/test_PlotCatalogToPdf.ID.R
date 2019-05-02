9070c28fd6c059fa0a17b8f565c143f46884c9e2
jnh <44010028+jnh01@users.noreply.github.com>
2019-05-02 18:50:23 +0800

    Update test_PlotCatalogToPdf.ID.R

--- tests/testthat/test_PlotCatalogToPdf.ID.R
+++ tests/testthat/test_PlotCatalogToPdf.ID.R
@@ -1,11 +1,24 @@
 context("PlotCatalogToPdf.ID")
 
 test_that("PlotCatalogToPdf.ID function is working properly", {
-  catalog <- ReadCatalog("testdata/BTSG_WGS_PCAWG.indels.csv",
+  catalog.counts <- ReadCatalog("testdata/BTSG_WGS_PCAWG.indels.csv",
                        ref.genome = "GRCh37",
                        region = "genome", catalog.type = "counts")
-  colnames(catalog) <- paste0("Biliary-AdenoCA", 1 : 35)
-  out <- PlotCatalogToPdf(catalog, filename = "PlotCatID.test.pdf")
+  colnames(catalog.counts) <- paste0("Biliary-AdenoCA", 1 : 35)
+  out <- PlotCatalogToPdf(catalog.counts, filename = "PlotCatID.test.pdf")
   expect_equal(out, TRUE)
+
+  catalog.counts.signature <-
+    apply(catalog.counts, MARGIN = 2, function(x) x / sum(x))
+  catalog.counts.signature <-
+    as.catalog(catalog.counts.signature, ref.genome = "GRCh37",
+               region = "genome", catalog.type = "counts.signature")
+  out <-
+    PlotCatalogToPdf(catalog.counts.signature,
+                     filename = "PlotCatID.counts.signature.test.pdf")
+  expect_equal(out, TRUE)
+
   unlink("PlotCatID.test.pdf")
+  unlink("PlotCatID.counts.signature.test.pdf")
+  graphics.off()
 })
