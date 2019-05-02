a34c51a8a9f9e54d777d7e73c5c4da0bbbc8b3eb
jnh <44010028+jnh01@users.noreply.github.com>
2019-05-02 18:54:12 +0800

    Update test_PlotCatalogToPdf.SNS192.R

--- tests/testthat/test_PlotCatalogToPdf.SNS192.R
+++ tests/testthat/test_PlotCatalogToPdf.SNS192.R
@@ -1,12 +1,43 @@
 context("PlotCatalogToPdf.SNS192")
 
 test_that("PlotCatalogToPdf.SNS192 function is working properly", {
-  catalog <- ReadCatalog("testdata/regress.cat.sns.192.csv", ref.genome = "GRCh37",
-                         region = "genome", catalog.type = "counts")
-  colnames(catalog) <- paste0("HepG2_", 1 : 4)
+  catalog.counts <- ReadCatalog("testdata/regress.cat.sns.192.csv",
+                                ref.genome = "GRCh37",
+                                region = "genome", catalog.type = "counts")
+  colnames(catalog.counts) <- paste0("HepG2_", 1 : 4)
   out <-
-    PlotCatalogToPdf(catalog, filename = "PlotCatSNS192.test.pdf")
+    PlotCatalogToPdf(catalog.counts, filename = "PlotCatSNS192.counts.test.pdf")
   expect_equal(out, TRUE)
-  unlink("PlotCatSNS192.test.pdf")
+
+  catalog.density <-
+    TransformCatalog(catalog.counts, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density")
+  out <-
+    PlotCatalogToPdf(catalog.density, filename = "PlotCatSNS192.density.test.pdf")
+  expect_equal(out, TRUE)
+
+  catalog.counts.signature <-
+    TransformCatalog(catalog.counts, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "counts.signature")
+  out <-
+    PlotCatalogToPdf(catalog.counts.signature,
+                     filename = "PlotCatSNS192.counts.signature.test.pdf")
+  expect_equal(out, TRUE)
+
+  catalog.density.signature <-
+    TransformCatalog(catalog.counts, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density.signature")
+  out <-
+    PlotCatalogToPdf(catalog.density.signature,
+                     filename = "PlotCatSNS192.density.signature.test.pdf")
+  expect_equal(out, TRUE)
+
+  unlink("PlotCatSNS192.counts.test.pdf")
+  unlink("PlotCatSNS192.density.test.pdf")
+  unlink("PlotCatSNS192.counts.signature.test.pdf")
+  unlink("PlotCatSNS192.density.signature.test.pdf")
   graphics.off()
 })
