738c987a686600033cba7e50cf1a6f41f5eed591
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-05-26 17:33:38 +0800

    Update create_testdata_for_AddSequence_and_AddAndCheckSequenceID.R

--- data-raw/create_testdata_for_AddSequence_and_AddAndCheckSequenceID.R
+++ data-raw/create_testdata_for_AddSequence_and_AddAndCheckSequenceID.R
@@ -5,23 +5,23 @@ cat(getwd(), "\n")
 list.of.vcf <- ReadAndSplitStrelkaSBSVCFs("data-raw/VCF/Strelka.SBS.GRCh37.vcf")
 sbs.vcf <- list.of.vcf$SBS.vcfs[[1]]
 strelka.SBS.vcf.GRCh37 <-
-  AddSequence(sbs.vcf, genome = BSgenome.Hsapiens.1000genomes.hs37d5)
+  AddSequence(sbs.vcf, ref.genome = BSgenome.Hsapiens.1000genomes.hs37d5)
 
 list.of.vcf <- ReadAndSplitStrelkaSBSVCFs("data-raw/VCF/Strelka.SBS.GRCh38.vcf")
 sbs.vcf <- list.of.vcf$SBS.vcfs[[1]]
 strelka.SBS.vcf.GRCh38 <-
-  AddSequence(sbs.vcf, genome = BSgenome.Hsapiens.UCSC.hg38)
+  AddSequence(sbs.vcf, ref.genome = BSgenome.Hsapiens.UCSC.hg38)
 
 save(strelka.SBS.vcf.GRCh37, strelka.SBS.vcf.GRCh38,
      file = "tests/testthat/testdata/test_AddSequence.Rdata")
 
 id.vcf <- ReadStrelkaIDVCF("data-raw/VCF/Strelka.ID.GRCh37.vcf")
 strelka.ID.vcf.GRCh37 <-
-  AddAndCheckSequenceID(id.vcf, genome = BSgenome.Hsapiens.1000genomes.hs37d5)
+  AddAndCheckSequenceID(id.vcf, ref.genome = BSgenome.Hsapiens.1000genomes.hs37d5)
 
 id.vcf <- ReadStrelkaIDVCF("data-raw/VCF/Strelka.ID.GRCh38.vcf")
 strelka.ID.vcf.GRCh38 <-
-  AddAndCheckSequenceID(id.vcf, genome = BSgenome.Hsapiens.UCSC.hg38)
+  AddAndCheckSequenceID(id.vcf, ref.genome = BSgenome.Hsapiens.UCSC.hg38)
 
 save(strelka.ID.vcf.GRCh37, strelka.ID.vcf.GRCh38,
-     file = "tests/testthat/testdata/test_AddSequence.Rdata")
+     file = "tests/testthat/testdata/test_AddAndCheckSequenceID.Rdata")
