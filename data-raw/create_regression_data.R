1577c61b526919cead40be1949a4b3f6bd44fada
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-05-26 16:32:40 +0800

    Update create_regression_data.R
    Changed the nomenclature for variant types: SNS -> SBS, DNS -> DBS

--- data-raw/create_regression_data.R
+++ data-raw/create_regression_data.R
@@ -8,19 +8,19 @@ files <- c("data-raw/VCF/HepG2_Cis_1_SNVresult_rmDup.vcf",
            "data-raw/VCF/HepG2_Cis_4_SNVresult_rmDup.vcf")
 
 catalog <-
-  StrelkaSNSVCFFilesToCatalog(files, genome = "GRCh37",
+  StrelkaSBSVCFFilesToCatalog(files, genome = "GRCh37",
                               trans.ranges = trans.ranges.GRCh37)
 
-WriteCatSNS96(catalog$catSNS96,
-              path = "tests/testthat/testdata/regress.cat.sns.96.csv")
-WriteCatSNS192(catalog$catSNS192,
-               path = "tests/testthat/testdata/regress.cat.sns.192.csv")
-WriteCatSNS1536(catalog$catSNS1536,
-                path = "tests/testthat/testdata/regress.cat.sns.1536.csv")
-WriteCatDNS78(catalog$catDNS78,
-              path = "tests/testthat/testdata/regress.cat.dns.78.csv")
-WriteCatDNS144(catalog$catDNS144,
-               path = "tests/testthat/testdata/regress.cat.dns.144.csv")
-WriteCatDNS136(catalog$catDNS136,
-               path = "tests/testthat/testdata/regress.cat.dns.136.csv")
+WriteCatSBS96(catalog$catSBS96,
+              path = "tests/testthat/testdata/regress.cat.sbs.96.csv")
+WriteCatSBS192(catalog$catSBS192,
+               path = "tests/testthat/testdata/regress.cat.sbs.192.csv")
+WriteCatSBS1536(catalog$catSBS1536,
+                path = "tests/testthat/testdata/regress.cat.sbs.1536.csv")
+WriteCatDBS78(catalog$catDBS78,
+              path = "tests/testthat/testdata/regress.cat.dbs.78.csv")
+WriteCatDBS144(catalog$catDBS144,
+               path = "tests/testthat/testdata/regress.cat.dbs.144.csv")
+WriteCatDBS136(catalog$catDBS136,
+               path = "tests/testthat/testdata/regress.cat.dbs.136.csv")
 
