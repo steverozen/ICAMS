0dd5aac049d2e3171d5e3f2bd6db73f4adf841b5
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-05-26 16:00:21 +0800

    Renamed PlotCatalog.SNSClassStrandBias.R to PlotCatalog.SNS192CatalogNoContext.R

--- tests/testthat/test_PlotCatalog.SNS192CatalogNoContext.R
+++ tests/testthat/test_PlotCatalog.SNS192CatalogNoContext.R
@@ -1,32 +1,32 @@
-context("PlotCatalog.SNSClassStrandBias")
+context("PlotCatalog.SNS192CatalogNoContext")
 
-test_that("PlotCatalog.SNSClassStrandBias function is working properly", {
+test_that("PlotCatalog.SNS192CatalogNoContext function is working properly", {
   par(mar = c(5, 5, 5, 1))
   catalog.counts <- ReadCatalog("testdata/regress.cat.sns.192.csv",
                                 ref.genome = "GRCh37",
                                 region = "genome", catalog.type = "counts")
   cat.counts <- catalog.counts[, 1, drop = FALSE]
-  out <- PlotCatalog(cat.counts, strandbias = TRUE)
+  out <- PlotCatalog(cat.counts, no.context = TRUE)
   expect_equal(out, TRUE)
 
   cat.density <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "density")
-  out <- PlotCatalog(cat.density, strandbias = TRUE)
+  out <- PlotCatalog(cat.density, no.context = TRUE)
   expect_equal(out, TRUE)
 
   cat.counts.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "counts.signature")
-  out <- PlotCatalog(cat.counts.signature, strandbias = TRUE)
+  out <- PlotCatalog(cat.counts.signature, no.context = TRUE)
   expect_equal(out, TRUE)
 
   cat.density.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "density.signature")
-  out <- PlotCatalog(cat.density.signature, strandbias = TRUE)
+  out <- PlotCatalog(cat.density.signature, no.context = TRUE)
   expect_equal(out, TRUE)
 })
