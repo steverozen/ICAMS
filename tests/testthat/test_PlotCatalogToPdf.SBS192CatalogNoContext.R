a23dd17d6a0988ef5534626f30be1f241a93de8e
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-05-26 17:13:43 +0800

    Update test_PlotCatalogToPdf.SBS192CatalogNoContext.R
    Changed the nomenclature for variant types: SNS -> SBS, DNS -> DBS

--- tests/testthat/test_PlotCatalogToPdf.SBS192CatalogNoContext.R
+++ tests/testthat/test_PlotCatalogToPdf.SBS192CatalogNoContext.R
@@ -1,13 +1,13 @@
-context("PlotCatalogToPdf.SNS192CatalogNoContext")
+context("PlotCatalogToPdf.SBS192CatalogNoContext")
 
-test_that("PlotCatalogToPdf.SNS192CatalogNoContext function is working properly", {
+test_that("PlotCatalogToPdf.SBS192CatalogNoContext function is working properly", {
   catalog.counts <- ReadCatalog("testdata/regress.cat.sbs.192.csv",
                                 ref.genome = "GRCh37",
                                 region = "genome", catalog.type = "counts")
   colnames(catalog.counts) <- paste0("HepG2_", 1 : 4)
   out <-
     PlotCatalogToPdf(catalog.counts,
-                     file = "PlotSNS192CatalogNoContext.counts.test.pdf",
+                     file = "PlotSBS192CatalogNoContext.counts.test.pdf",
                      no.context = TRUE)
   expect_equal(out, TRUE)
 
@@ -17,7 +17,7 @@ test_that("PlotCatalogToPdf.SNS192CatalogNoContext function is working properly"
                      target.catalog.type = "density")
   out <-
     PlotCatalogToPdf(catalog.density,
-                     file = "PlotSNS192CatalogNoContext.density.test.pdf",
+                     file = "PlotSBS192CatalogNoContext.density.test.pdf",
                      no.context = TRUE)
   expect_equal(out, TRUE)
 
@@ -27,7 +27,7 @@ test_that("PlotCatalogToPdf.SNS192CatalogNoContext function is working properly"
                      target.catalog.type = "counts.signature")
   out <-
     PlotCatalogToPdf(catalog.counts.signature,
-                     file = "PlotSNS192CatalogNoContext.counts.signature.test.pdf",
+                     file = "PlotSBS192CatalogNoContext.counts.signature.test.pdf",
                      no.context = TRUE)
   expect_equal(out, TRUE)
 
@@ -37,13 +37,13 @@ test_that("PlotCatalogToPdf.SNS192CatalogNoContext function is working properly"
                      target.catalog.type = "density.signature")
   out <-
     PlotCatalogToPdf(catalog.density.signature,
-                     file = "PlotSNS192CatalogNoContext.density.signature.test.pdf",
+                     file = "PlotSBS192CatalogNoContext.density.signature.test.pdf",
                      no.context = TRUE)
   expect_equal(out, TRUE)
 
-  unlink("PlotSNS192CatalogNoContext.counts.test.pdf")
-  unlink("PlotSNS192CatalogNoContext.density.test.pdf")
-  unlink("PlotSNS192CatalogNoContext.counts.signature.test.pdf")
-  unlink("PlotSNS192CatalogNoContext.density.signature.test.pdf")
+  unlink("PlotSBS192CatalogNoContext.counts.test.pdf")
+  unlink("PlotSBS192CatalogNoContext.density.test.pdf")
+  unlink("PlotSBS192CatalogNoContext.counts.signature.test.pdf")
+  unlink("PlotSBS192CatalogNoContext.density.signature.test.pdf")
   graphics.off()
 })
