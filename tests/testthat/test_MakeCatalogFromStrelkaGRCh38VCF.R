6db130bc8e5dcaa0bf15ae8334304b1c6533768a
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-05-26 16:50:09 +0800

    Update test_MakeCatalogFromStrelkaGRCh38VCF.R
    Changed the nomenclature for variant types: SNS -> SBS, DNS -> DBS

--- tests/testthat/test_MakeCatalogFromStrelkaGRCh38VCF.R
+++ tests/testthat/test_MakeCatalogFromStrelkaGRCh38VCF.R
@@ -1,20 +1,20 @@
 context("Making catalogs from Strelka GRCh38 VCFs")
 
-test_that("StrelkaSNSVCFFilesToCatalog", {
-  cat1 <- StrelkaSNSVCFFilesToCatalog("testdata/Strelka.SNS.GRCh38.vcf",
+test_that("StrelkaSBSVCFFilesToCatalog", {
+  cat1 <- StrelkaSBSVCFFilesToCatalog("testdata/Strelka.SBS.GRCh38.vcf",
                                       ref.genome = BSgenome.Hsapiens.UCSC.hg38,
                                       trans.ranges = trans.ranges.GRCh38,
                                       region = "genome")
-  cat2 <- StrelkaSNSVCFFilesToCatalog("testdata/Strelka.SNS.GRCh38.vcf",
+  cat2 <- StrelkaSBSVCFFilesToCatalog("testdata/Strelka.SBS.GRCh38.vcf",
                                       ref.genome = "GRCh38",
                                       trans.ranges = trans.ranges.GRCh38,
                                       region = "genome")
-  cat3 <- StrelkaSNSVCFFilesToCatalog("testdata/Strelka.SNS.GRCh38.vcf",
+  cat3 <- StrelkaSBSVCFFilesToCatalog("testdata/Strelka.SBS.GRCh38.vcf",
                                       ref.genome = "hg38",
                                       trans.ranges = trans.ranges.GRCh38,
                                       region = "genome")
-  expect_equal(cat1$catSNS96, cat2$catSNS96)
-  expect_equal(cat1$catSNS96, cat3$catSNS96)
+  expect_equal(cat1$catSBS96, cat2$catSBS96)
+  expect_equal(cat1$catSBS96, cat3$catSBS96)
 })
 
 test_that("StrelkaIDVCFFilesToCatalog", {
