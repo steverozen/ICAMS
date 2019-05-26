d682ae993b9c91c1485e2c38ba6b6fb09f4cd74d
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-05-26 17:06:20 +0800

    Update test_AddSequence.R
    Changed the nomenclature for variant types: SNS -> SBS, DNS -> DBS

--- tests/testthat/test_AddSequence.R
+++ tests/testthat/test_AddSequence.R
@@ -3,10 +3,10 @@ context("AddSequence")
 test_that("AddSequence for GRCh37", {
   load("testdata/test_AddSequence.Rdata")
   list.of.vcf <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
-  sns.vcf <- list.of.vcf$SBS.vcfs[[1]]
-  df <- AddSequence(sns.vcf, ref.genome = BSgenome.Hsapiens.1000genomes.hs37d5)
-  df1 <- AddSequence(sns.vcf, ref.genome = "GRCh37")
-  df2 <- AddSequence(sns.vcf, ref.genome = "hg19")
+  sbs.vcf <- list.of.vcf$SBS.vcfs[[1]]
+  df <- AddSequence(sbs.vcf, ref.genome = BSgenome.Hsapiens.1000genomes.hs37d5)
+  df1 <- AddSequence(sbs.vcf, ref.genome = "GRCh37")
+  df2 <- AddSequence(sbs.vcf, ref.genome = "hg19")
   expect_equal(df, strelka.SBS.vcf.GRCh37)
   expect_equal(df, df1)
   expect_equal(df, df2)
@@ -15,10 +15,10 @@ test_that("AddSequence for GRCh37", {
 test_that("AddSequence for GRCh38", {
   load("testdata/test_AddSequence.Rdata")
   list.of.vcf <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh38.vcf")
-  sns.vcf <- list.of.vcf$SBS.vcfs[[1]]
-  df3 <- AddSequence(sns.vcf, ref.genome = BSgenome.Hsapiens.UCSC.hg38)
-  df4 <- AddSequence(sns.vcf, ref.genome = "GRCh38")
-  df5 <- AddSequence(sns.vcf, ref.genome = "hg38")
+  sbs.vcf <- list.of.vcf$SBS.vcfs[[1]]
+  df3 <- AddSequence(sbs.vcf, ref.genome = BSgenome.Hsapiens.UCSC.hg38)
+  df4 <- AddSequence(sbs.vcf, ref.genome = "GRCh38")
+  df5 <- AddSequence(sbs.vcf, ref.genome = "hg38")
   expect_equal(df3, strelka.SBS.vcf.GRCh38)
   expect_equal(df3, df4)
   expect_equal(df3, df5)
