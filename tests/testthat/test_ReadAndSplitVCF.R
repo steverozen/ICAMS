e8e2127d879b390217d65ee5f2cf5de88d32e355
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-05-26 17:02:00 +0800

    Update test_ReadAndSplitVCF.R
    Changed the nomenclature for variant types: SNS -> SBS, DNS -> DBS

--- tests/testthat/test_ReadAndSplitVCF.R
+++ tests/testthat/test_ReadAndSplitVCF.R
@@ -1,10 +1,10 @@
 context("Reading and splitting VCFs")
 
 test_that(
-  "ReadStrelkaSNSVCFs",
+  "ReadStrelkaSBSVCFs",
   {
-    vcf <- ReadStrelkaSNSVCFs("testdata/Strelka.SNS.GRCh37.vcf")
-    vcf1 <- ReadStrelkaSNSVCFs("testdata/Strelka.SNS.GRCh38.vcf")
+    vcf <- ReadStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
+    vcf1 <- ReadStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh38.vcf")
     expect_equal(dim(vcf[[1]]), c(798, 20))
     expect_equal(dim(vcf1[[1]]), c(992, 12))
   } )
@@ -25,9 +25,9 @@ test_that(
   } )
 
 test_that(
-  "ReadStrelkaSNSVCFs applied to Mutect VCF error",
+  "ReadStrelkaSBSVCFs applied to Mutect VCF error",
   {
-    expect_error(ReadStrelkaSNSVCFs("testdata/Mutect.GRCh37.vcf"),
+    expect_error(ReadStrelkaSBSVCFs("testdata/Mutect.GRCh37.vcf"),
                  "does not appear to be a Strelka VCF",
                  fixed = TRUE)
   })
@@ -35,7 +35,7 @@ test_that(
 test_that(
   "ReadMutectVCFs applied to Strelka VCF error",
   {
-    expect_error(ReadMutectVCFs("testdata/Strelka.SNS.GRCh37.vcf"),
+    expect_error(ReadMutectVCFs("testdata/Strelka.SBS.GRCh37.vcf"),
                  "does not appear to be a Mutect VCF",
                  fixed = TRUE)
   }
