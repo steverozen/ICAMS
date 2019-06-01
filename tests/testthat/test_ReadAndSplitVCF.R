0c599a8b44aa84eeef6020ce1774dd4bf01953c7
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-06-01 16:01:04 +0800

    Update test_ReadAndSplitVCF.R

--- tests/testthat/test_ReadAndSplitVCF.R
+++ tests/testthat/test_ReadAndSplitVCF.R
@@ -6,7 +6,7 @@ test_that(
     vcf <- ReadStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
     vcf1 <- ReadStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh38.vcf")
     expect_equal(dim(vcf[[1]]), c(798, 20))
-    expect_equal(dim(vcf1[[1]]), c(992, 12))
+    expect_equal(dim(vcf1[[1]]), c(1574, 12))
   } )
 
 test_that(
@@ -15,7 +15,7 @@ test_that(
     vcf <- ReadStrelkaIDVCFs("testdata/Strelka.ID.GRCh37.vcf")
     vcf1 <- ReadStrelkaIDVCFs("testdata/Strelka.ID.GRCh38.vcf")
     expect_equal(dim(vcf[[1]]), c(408, 19))
-    expect_equal(dim(vcf1[[1]]), c(399, 19))
+    expect_equal(dim(vcf1[[1]]), c(1574, 11))
   } )
 
 test_that(
@@ -30,12 +30,20 @@ test_that(
     expect_error(ReadStrelkaSBSVCFs("testdata/Mutect.GRCh37.vcf"),
                  "does not appear to be a Strelka VCF",
                  fixed = TRUE)
+
+    expect_error(ReadStrelkaSBSVCFs("testdata/Mutect.GRCh38.vcf"),
+                 "does not appear to be a Strelka VCF",
+                 fixed = TRUE)
   })
 
 test_that(
   "ReadMutectVCFs applied to Strelka VCF error",
   {
     expect_error(ReadMutectVCFs("testdata/Strelka.SBS.GRCh37.vcf"),
+                 "does not appear to be a Mutect VCF",
+                 fixed = TRUE)
+
+    expect_error(ReadMutectVCFs("testdata/Strelka.SBS.GRCh38.vcf"),
                  "does not appear to be a Mutect VCF",
                  fixed = TRUE)
   }
