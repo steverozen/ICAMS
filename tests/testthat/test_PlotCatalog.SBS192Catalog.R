3e60a6ef29da425231f9e27f53d74475f0f71905
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-06-02 18:28:48 +0800

    Create test_PlotCatalog.SBS192Catalog.R

---
+++ tests/testthat/test_PlotCatalog.SBS192Catalog.R
@@ -0,0 +1,40 @@
+context("PlotCatalog.SBS192Catalog")
+
+test_that("PlotCatalog.SBS192Catalog function is working properly", {
+  par(mar = c(2, 2, 2, 1))
+  catalog.counts <-
+    ReadCatalog("testdata/regress.cat.sbs.192.csv", ref.genome = "GRCh37",
+                         region = "genome", catalog.type = "counts")
+  cat.counts <- catalog.counts[, 1, drop = FALSE]
+  out <- PlotCatalog(cat.counts)
+  out1 <- PlotCatalog(cat.counts, no.context = TRUE)
+  expect_equal(out, TRUE)
+  expect_equal(out1, TRUE)
+
+  cat.density <-
+    TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density")
+  out <- PlotCatalog(cat.density)
+  out1 <- PlotCatalog(cat.density, no.context = TRUE)
+  expect_equal(out, TRUE)
+  expect_equal(out1, TRUE)
+
+  cat.counts.signature <-
+    TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "counts.signature")
+  out <- PlotCatalog(cat.counts.signature)
+  out1 <- PlotCatalog(cat.counts.signature, no.context = TRUE)
+  expect_equal(out, TRUE)
+  expect_equal(out1, TRUE)
+
+  cat.density.signature <-
+    TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density.signature")
+  out <- PlotCatalog(cat.density.signature)
+  out1 <- PlotCatalog(cat.density.signature, no.context = TRUE)
+  expect_equal(out, TRUE)
+  expect_equal(out1, TRUE)
+  })
