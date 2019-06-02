be27f5f7b629e46debaf63e27feb184e843f4028
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-06-02 18:29:42 +0800

    Update test_PlotCatalog.DBS144Catalog.R

--- tests/testthat/test_PlotCatalog.DBS144Catalog.R
+++ tests/testthat/test_PlotCatalog.DBS144Catalog.R
@@ -1,7 +1,7 @@
 context("PlotCatalog.DBS144Catalog")
 
 test_that("PlotCatalog.DBS144Catalog function is working properly", {
-  par(mar = c(5, 8, 5, 1))
+  par(mar = c(1, 1, 1, 1))
   catalog.counts <- ReadCatalog("testdata/regress.cat.dbs.144.csv",
                                 ref.genome = "GRCh37",
                                 region = "genome", catalog.type = "counts")
