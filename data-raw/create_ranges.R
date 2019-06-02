bbf43fc7d058a9685e81a704096b45ffa3454759
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-06-02 18:48:18 +0800

    Update create_ranges.R

--- data-raw/create_ranges.R
+++ data-raw/create_ranges.R
@@ -8,16 +8,18 @@ trans.ranges.GRCh37 <-
 trans.ranges.GRCh38 <-
   ReadTranscriptRanges("data-raw/ranges/TranscriptRanges.GRCh38.GENECODEv30.csv")
 
-exome.ranges.GRCh37 <-
-  ReadBedRanges("data-raw/ranges/ExomeRanges.GRCh37.SureSelectv6.csv")
+if (FALSE) {
+  exome.ranges.GRCh37 <-
+    ReadBedRanges("data-raw/ranges/ExomeRanges.GRCh37.SureSelectv6.csv")
 
-exome.ranges.stranded.GRCh37 <-
-  CreateExomeStrandedRanges(path = "data-raw/ranges/ExomeRanges.GRCh37.SureSelectv6.csv",
-                            trans.ranges = trans.ranges.GRCh37)
+  exome.ranges.stranded.GRCh37 <-
+    CreateExomeStrandedRanges(path = "data-raw/ranges/ExomeRanges.GRCh37.SureSelectv6.csv",
+                              trans.ranges = trans.ranges.GRCh37)
 
-exome.ranges.GRCh38 <-
-  ReadBedRanges("data-raw/ranges/ExomeRanges.GRCh38.SureSelectv6.csv")
+  exome.ranges.GRCh38 <-
+    ReadBedRanges("data-raw/ranges/ExomeRanges.GRCh38.SureSelectv6.csv")
 
-exome.ranges.stranded.GRCh38 <-
-  CreateExomeStrandedRanges(path = "data-raw/ranges/ExomeRanges.GRCh38.SureSelectv6.csv",
-                            trans.ranges = trans.ranges.GRCh38)
+  exome.ranges.stranded.GRCh38 <-
+    CreateExomeStrandedRanges(path = "data-raw/ranges/ExomeRanges.GRCh38.SureSelectv6.csv",
+                              trans.ranges = trans.ranges.GRCh38)
+}
