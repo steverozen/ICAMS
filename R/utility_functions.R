7e7e468cd24ef02323c65bac0b343150888b28c7
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-06-02 23:15:12 +0800

    Updated function CreateCatalogAbundance

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -828,10 +828,18 @@ CreateCatalogAbundance <- function(catalog, ref.genome, region, catalog.type) {
       return(catalog)
     } else if (ref.genome %in%
                c("GRCh37", "hg19", "BSgenome.Hsapiens.1000genomes.hs37d5")) {
+      if (region == "genome") {
         attr(catalog, "abundance") <- abundance.3bp.genome.stranded.GRCh37
+      } else if (region == "exome") {
+        attr(catalog, "abundance") <- abundance.3bp.exome.stranded.GRCh37
+      }
     } else if (ref.genome %in%
                c("GRCh38", "hg38", "BSgenome.Hsapiens.UCSC.hg38")) {
+      if (region == "genome") {
         attr(catalog, "abundance") <- abundance.3bp.genome.stranded.GRCh38
+      } else if (region == "exome") {
+        attr(catalog, "abundance") <- abundance.3bp.exome.stranded.GRCh38
+      }
     }
   }
 
@@ -887,10 +895,18 @@ CreateCatalogAbundance <- function(catalog, ref.genome, region, catalog.type) {
       return(catalog)
     } else if (ref.genome %in%
                c("GRCh37", "hg19", "BSgenome.Hsapiens.1000genomes.hs37d5")) {
+      if (region == "genome") {
         attr(catalog, "abundance") <- abundance.2bp.genome.stranded.GRCh37
+      } else if (region == "exome") {
+        attr(catalog, "abundance") <- abundance.2bp.exome.stranded.GRCh37
+      }
     } else if (ref.genome %in%
                c("GRCh38", "hg38", "BSgenome.Hsapiens.UCSC.hg38")) {
+      if (region == "genome") {
         attr(catalog, "abundance") <- abundance.2bp.genome.stranded.GRCh38
+      } else if (region == "exome") {
+        attr(catalog, "abundance") <- abundance.2bp.exome.stranded.GRCh38
+      }
     }
   }
 
