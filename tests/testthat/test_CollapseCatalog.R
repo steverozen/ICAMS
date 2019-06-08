1b1b8ecdedf481c4952538ea256a786c24645cec
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-06-08 11:36:08 +0800

    Update test_CollapseCatalog.R

--- tests/testthat/test_CollapseCatalog.R
+++ tests/testthat/test_CollapseCatalog.R
@@ -8,11 +8,29 @@ test_that("Collapse1536CatalogTo96 function is working properly", {
   expect_equal(colSums(cat.SBS1536), colSums(x1))
   expect_equal("SBS96Catalog", class(x1)[1])
 
+  cat.SBS1536.density <-
+    TransformCatalog(cat.SBS1536, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density")
+  x2 <- Collapse1536CatalogTo96(cat.SBS1536.density)
+  expect_equal(colSums(cat.SBS1536.density), colSums(x2))
+  expect_equal("SBS96Catalog", class(x2)[1])
+
   cat.SBS1536.counts.signature <-
     TransformCatalog(cat.SBS1536, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "counts.signature")
-  expect_error(Collapse1536CatalogTo96(cat.SBS1536.counts.signature))
+  x3 <- Collapse1536CatalogTo96(cat.SBS1536.counts.signature)
+  expect_equal(colSums(cat.SBS1536.counts.signature), colSums(x3))
+  expect_equal("SBS96Catalog", class(x3)[1])
+
+  cat.SBS1536.density.signature <-
+    TransformCatalog(cat.SBS1536, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density.signature")
+  x4 <- Collapse1536CatalogTo96(cat.SBS1536.density.signature)
+  expect_equal(colSums(cat.SBS1536.density.signature), colSums(x4))
+  expect_equal("SBS96Catalog", class(x4)[1])
 })
 
 test_that("Collapse192CatalogTo96 function is working properly", {
@@ -23,11 +41,29 @@ test_that("Collapse192CatalogTo96 function is working properly", {
   expect_equal(colSums(cat.SBS192), colSums(x1))
   expect_equal("SBS96Catalog", class(x1)[1])
 
+  cat.SBS192.density <-
+    TransformCatalog(cat.SBS192, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density")
+  x2 <- Collapse192CatalogTo96(cat.SBS192.density)
+  expect_equal(colSums(cat.SBS192.density), colSums(x2))
+  expect_equal("SBS96Catalog", class(x2)[1])
+
   cat.SBS192.counts.signature <-
     TransformCatalog(cat.SBS192, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "counts.signature")
-  expect_error(Collapse192CatalogTo96(cat.SBS192.counts.signature))
+  x3 <- Collapse192CatalogTo96(cat.SBS192.counts.signature)
+  expect_equal(colSums(cat.SBS192.counts.signature), colSums(x3))
+  expect_equal("SBS96Catalog", class(x3)[1])
+
+  cat.SBS192.density.signature <-
+    TransformCatalog(cat.SBS192, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density.signature")
+  x4 <- Collapse192CatalogTo96(cat.SBS192.density.signature)
+  expect_equal(colSums(cat.SBS192.density.signature), colSums(x4))
+  expect_equal("SBS96Catalog", class(x4)[1])
 })
 
 test_that("Collapse144CatalogTo78 function is working properly", {
@@ -38,9 +74,27 @@ test_that("Collapse144CatalogTo78 function is working properly", {
   expect_equal(colSums(cat.DBS144), colSums(x1))
   expect_equal("DBS78Catalog", class(x1)[1])
 
+  cat.DBS144.density <-
+    TransformCatalog(cat.DBS144, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density")
+  x2 <- Collapse144CatalogTo78(cat.DBS144.density)
+  expect_equal(colSums(cat.DBS144.density), colSums(x2))
+  expect_equal("DBS78Catalog", class(x2)[1])
+
   cat.DBS144.counts.signature <-
     TransformCatalog(cat.DBS144, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "counts.signature")
-  expect_error(Collapse144CatalogTo78(cat.DBS144.counts.signature))
+  x3 <- Collapse144CatalogTo78(cat.DBS144.counts.signature)
+  expect_equal(colSums(cat.DBS144.counts.signature), colSums(x3))
+  expect_equal("DBS78Catalog", class(x3)[1])
+
+  cat.DBS144.density.signature <-
+    TransformCatalog(cat.DBS144, target.ref.genome = "GRCh37",
+                     target.region = "genome",
+                     target.catalog.type = "density.signature")
+  x4 <- Collapse144CatalogTo78(cat.DBS144.density.signature)
+  expect_equal(colSums(cat.DBS144.density.signature), colSums(x4))
+  expect_equal("DBS78Catalog", class(x4)[1])
 })
