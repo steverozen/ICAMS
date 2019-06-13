f8903d23885741784210c921fff9565dbb98cbd5
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-06-13 22:50:46 +0800

    Added new tests for the case region = "unknown" and the case where trans.ranges = NULL

--- tests/testthat/test_MakeCatalogFromStrelkaGRCh38VCF.R
+++ tests/testthat/test_MakeCatalogFromStrelkaGRCh38VCF.R
@@ -13,21 +13,30 @@ test_that("StrelkaSBSVCFFilesToCatalog", {
                                       ref.genome = "hg38",
                                       trans.ranges = trans.ranges.GRCh38,
                                       region = "genome")
+  cat4 <- StrelkaSBSVCFFilesToCatalog("testdata/Strelka.SBS.GRCh38.vcf",
+                                      ref.genome = "hg38")
   expect_equal(cat1$catSBS96, cat2$catSBS96)
   expect_equal(cat1$catSBS96, cat3$catSBS96)
+  expect_equal(attributes(cat4$catSBS96)$region, "unknown")
+  expect_null(cat4$catSBS192)
+  expect_null(cat4$catDBS144)
+  expect_null(attributes(cat4$catSBS96)$abundance)
 })
 
 test_that("StrelkaIDVCFFilesToCatalog", {
-  cat4 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
+  cat5 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
                                      ref.genome = BSgenome.Hsapiens.UCSC.hg38,
                                      region = "genome")
-  cat5 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
+  cat6 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
                                      ref.genome = "GRCh38",
                                      region = "genome")
-  cat6 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
+  cat7 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
                                      ref.genome = "hg38",
                                      region = "genome")
-  expect_equal(cat4, cat5)
-  expect_equal(cat4, cat6)
-
+  cat8 <- StrelkaIDVCFFilesToCatalog("testdata/Strelka.ID.GRCh38.vcf",
+                                     ref.genome = "hg38")
+  expect_equal(cat5, cat6)
+  expect_equal(cat5, cat7)
+  expect_equal(attributes(cat8)$region, "unknown")
+  expect_null(attributes(cat8)$abundance)
 })
