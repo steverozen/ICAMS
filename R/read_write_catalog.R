32cbf0ef4710ee9c7e46c3f350e1574aa14fd3f9
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-06-21 22:16:32 +0800

    Added example to function WriteCatalog()

--- R/read_write_catalog.R
+++ R/read_write_catalog.R
@@ -59,6 +59,15 @@ ReadCatalog <- function(file, ref.genome, region, catalog.type, strict = TRUE) {
 #'   deletion repeat sizes range from 1 to 6+.
 #'
 #' @export
+#' 
+#' @examples 
+#' file <- system.file("extdata",
+#'                     "strelka.regress.cat.sbs.96.csv",
+#'                     package = "ICAMS")
+#' catSBS96 <- ReadCatalog(file, ref.genome = "hg19", 
+#'                         region = "genome",
+#'                         catalog.type = "counts")
+#' WriteCatalog(catSBS96, file = paste0(tempdir(), "\\catSBS96.csv"))
 WriteCatalog <- function(catalog, file, strict = TRUE) {
   UseMethod(generic = "WriteCatalog")
 }
