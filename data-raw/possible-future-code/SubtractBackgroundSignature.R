fbfd3b422ecc88daf02c34d4a7e1fedf506c5115
steverozen <steve@steverozen.net>
2019-06-21 16:54:33 +0800

    sketch of future code

--- data-raw/possible-future-code/SubtractBackgroundSignature.R
+++ data-raw/possible-future-code/SubtractBackgroundSignature.R
@@ -1,44 +1,74 @@
 #' @keywords internal
-SubtractBackground1 <- function(spectrum, background) {
-     
+LikelihoodBackgroundOnly <- function(spectra, background.sig) {
+     # Find max likelihood and count to generate each
+     # spectrum in spectra using only background (the model could contain 
+     # an expected distribution of intensity of background mutagenesis)
   
+     # will compare this with a model with another signature
+     # (the same across all samples) with variable count
+     # per sample
+  
+     # To find the background signature, find the shape and
+     # and intensity that gives the observed backgrounds.
   
 }
 
 
 
-#' Subtract a background signature of unknown intensity from mutational specta
+#' Subtract background if required by likelihood ratio test for presenece of non-backgound signature
 #' 
 #' @param spectra An \code{ICAMS}
 #'  \code{counts} or \code{density} \code{catalog}.
 #'
-#' @param background An \code{ICAMS} 
+#' @param background.sig An \code{ICAMS} 
 #'  \code{counts.signature} or \code{density.signature} \code{catalog}. 
 #'  
 #'  \itemize{
 #'  
 #'  \item If
-#'  the class and \code{background} and \code{catalog}
+#'  the class and \code{background.sig} and \code{catalog}
 #'  must be the same, and
 #'  
 #'  \item if \code{catalog.type} of
-#'  \code{background} is \code{density.signature} then the \code{catalog.type}
+#'  \code{background.sig} is \code{density.signature} then the \code{catalog.type}
 #'  of \code{spectra} must be \code{density}, and
 #'  
 #'  \item If the \code{catalog.type} of
-#'  \code{background} is \code{counts.signature} then the \code{catalog.type}
+#'  \code{background.sig} is \code{counts.signature} then the \code{catalog.type}
 #'  of \code{spectra} must be \code{counts} \strong{and} the
 #'  \code{abundance} attributes must match.
 #'  
 #'  } 
 #'  
 #' @export
-SubtractBackgroundSignature <- function(spectra, background) {
+LSubtractBackground <- function(spectra, background.sig) {
+  out.spectra <- 
+    apply(X = colnames(spectra),
+          MARGIN = 2,
+          function(spectrum) {
+            SubtractBackground1(spectrum, background.sig, background.count)
+          }
+    )
+  return(out.spectra) # Not sure how to make this a catalog again
+}
+
+
+
+
+#' Subtract background if required by a mutation count test methods
+#' 
+#' @inheritParams LSubtractBackground
+#'
+#' @param background.count Expected total number of mutatations due to 
+#' \code{background.sig}.
+#'  
+#' @export
+CSubtractBackground <- function(spectra, background.sig, background.count) {
     out.spectra <- 
       apply(X = colnames(spectra),
             MARGIN = 2,
-            function(spectrum, signature) {
-              SubtractBackground1(spectrum, signature)
+            function(spectrum) {
+              SubtractBackground1(spectrum, background.sig, background.count)
             }
       )
     return(out.spectra) # Not sure how to make this a catalog again
