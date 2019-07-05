675497d5b5332926e1bb61078aaf55fb55b48a82
steverozen <steve@steverozen.net>
2019-07-05 16:39:27 +0800

    Added another private utility function for reading other catalog formats.

--- R/other_catalog_formats.R
+++ R/other_catalog_formats.R
@@ -28,3 +28,39 @@ ReadDukeNUSCat192 <- function(file,
   list(cat96 = cat96, cat192 = cat192)
 }
 
+#' Read a 96-channel spectra (or signature) catalog where rownames are e.g. "A[C>A]T"
+#' 
+#' The file needs to have the rownames in the first column.
+#' @keywords internal
+#' @importFrom utils read.table
+
+ReadStapleGT96SBS <- function(file,
+                              ref.genome   = NULL,
+                              region       = "unknown",
+                              catalog.type = "counts",
+                              abundance    = NULL,
+                              sep          = "\t") {
+  df <- read.table(file,
+                   stringsAsFactors = FALSE,
+                   as.is            = TRUE,
+                   header           = TRUE, 
+                   check.names      = FALSE,
+                   sep              = sep)
+  # Careful, df has 96 rows
+  stopifnot(nrow(df)==96)
+  c1 <- df[ , 1]
+  
+  # E.g. "A[C>A]T" -> "ACTA"
+  rn <-
+    paste0(substr(c1, 1, 1),
+           substr(c1, 3, 3),
+           substr(c1, 7, 7),
+           substr(c1, 5, 5))
+  
+  rownames(df) <- rn
+  df <- df[ , -1]
+  df.cat <- as.catalog(df, ref.genome = ref.genome, region = region,
+                       catalog.type = catalog.type, abundance = abundance)
+  return(df.cat)
+}
+
