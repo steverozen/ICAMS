e46edc6ea11bb9bfa339be923b69ec1f04ec25a5
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-07-16 21:52:30 +0800

    Update test_extended_seq.context_functions.R

--- tests/testthat/test_extended_seq.context_functions.R
+++ tests/testthat/test_extended_seq.context_functions.R
@@ -32,6 +32,22 @@ test_that("extended seq.context functions for Mutect GRCh37 vcf", {
   unlink(file.path(tempdir(), "Extended.seq.context.21bases.pdf"))
 })
 
+test_that("extended seq.context functions for Mutect GRCm38 vcf", {
+  skip_if("" == system.file(package = "BSgenome.Mmusculus.UCSC.mm10"))
+  stopifnot(requireNamespace("BSgenome.Mmusculus.UCSC.mm10"))
+  vcf <- ReadAndSplitMutectVCFs("testdata/Mutect.GRCm38.vcf")
+  sbs.vcf <- vcf$SBS[[1]]
+  mat <- CreateOnePPMFromSBSVCF(sbs.vcf, ref.genome = "GRCm38",
+                                seq.context.width = 10)
+  out <- PlotPPM(mat, title = "ExtendedSeqContext_21bases")
+  out1 <- 
+    PlotPPMToPdf(list(mat), titles = "ExtendedSeqContext_21bases",
+                 file = file.path(tempdir(), "Extended.seq.context.21bases.pdf"))
+  expect_equal(out, TRUE)
+  expect_equal(out1, TRUE)
+  unlink(file.path(tempdir(), "Extended.seq.context.21bases.pdf"))
+})
+
 test_that("extended seq.context functions for Strelka GRCh38 vcf", {
   skip_if("" == system.file(package = "BSgenome.Hsapiens.UCSC.hg38"))
   stopifnot(requireNamespace("BSgenome.Hsapiens.UCSC.hg38"))
