5fa48a0853b31e64b2befbce47ebde8c9350a1e2
nat <nattzy94@gmail.com>
2019-09-03 17:44:13 +0800

    code for dist2TSS function

---
+++ R/dist2TSS.R
@@ -0,0 +1,4 @@
+vcfs <- ReadAndSplitStrelkaSBSVCFs("/Users/nat/Documents/ICAMS-dev/HepG2_Cis_1_SNVresult_rmDup2.vcf")
+sbs.vcf <- vcfs$SBS.vcfs[[1]]
+annotated.vcf <- AnnotateSBSVCF(SBS.vcf = sbs.vcf, ref.genome = "hg19", trans.ranges = trans.ranges.GRCh37)
+
