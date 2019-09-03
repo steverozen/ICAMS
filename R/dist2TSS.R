931fc75d900c94ec6b6dfe965fc9af0bbea6e750
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-09-03 18:34:08 +0800

    Update dist2TSS.R

--- R/dist2TSS.R
+++ R/dist2TSS.R
@@ -1,4 +1,10 @@
-vcfs <- ReadAndSplitStrelkaSBSVCFs("/Users/nat/Documents/ICAMS-dev/HepG2_Cis_1_SNVresult_rmDup2.vcf")
-sbs.vcf <- vcfs$SBS.vcfs[[1]]
-annotated.vcf <- AnnotateSBSVCF(SBS.vcf = sbs.vcf, ref.genome = "hg19", trans.ranges = trans.ranges.GRCh37)
+
+# Check the current working directory
+if (grepl("nat", getwd())) {
+  vcfs <- ReadAndSplitStrelkaSBSVCFs("/Users/nat/Documents/ICAMS-dev/HepG2_Cis_1_SNVresult_rmDup2.vcf")
+  sbs.vcf <- vcfs$SBS.vcfs[[1]]
+  annotated.vcf <- AnnotateSBSVCF(SBS.vcf = sbs.vcf, ref.genome = "hg19", 
+                                  trans.ranges = trans.ranges.GRCh37)
+}
+
 
