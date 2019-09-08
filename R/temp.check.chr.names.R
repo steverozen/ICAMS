d7a1331bd9b147268f0c9db6259d3349d07a018d
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-09-08 12:34:52 +0800

    Updated function CheckAndFixChrNames()

--- R/temp.check.chr.names.R
+++ R/temp.check.chr.names.R
@@ -28,9 +28,13 @@
 #' @export
 
 CheckAndFixChrNames <- function(vcf.df, ref.genome) {
-  
   names.to.check <- unique(vcf.df$CHROM)
-
+  # Check whether the naming of chromosomes in vcf.df is consistent
+  if(!sum(grepl("^chr", names.to.check)) %in% c(0, length(names.to.check))) {
+    stop("Naming of chromosomes in input is not consistent: ",
+         paste(names.to.check, collapse = " "))
+  }
+  
   ref.genome.names <- seqnames(ref.genome)
   
   not.matched <- setdiff(names.to.check, ref.genome.names)
@@ -38,12 +42,9 @@ CheckAndFixChrNames <- function(vcf.df, ref.genome) {
   # The names match -- we leave well-enough alone
   if (length(not.matched) == 0) return(vcf.df$CHROM)
   
-  chr.index <- grep(pattern = "^chr", names.to.check)
-  vcf.has.chr.prefix <- length(chr.index) != 0
+  vcf.has.chr.prefix <- any(grepl(pattern = "^chr", names.to.check))
+  ref.has.chr.prefix <- any(grepl(pattern = "^chr", ref.genome.names))
   
-  chr.index <- grep(pattern = "^chr", ref.genome.names)
-  ref.has.chr.prefix <- length(chr.index) != 0
-
   new.chr.names <- vcf.df$CHROM
   if (ref.has.chr.prefix && !vcf.has.chr.prefix) {
     names.to.check <- paste0("chr", names.to.check)
@@ -51,12 +52,19 @@ CheckAndFixChrNames <- function(vcf.df, ref.genome) {
     not.matched1 <- setdiff(names.to.check, ref.genome.names)
     if (length(not.matched1) == 0) return(new.chr.names)
   }
-
+  
+  if (!ref.has.chr.prefix && vcf.has.chr.prefix) {
+    names.to.check <- gsub("chr", "", names.to.check)
+    new.chr.names <- gsub("chr", "", names.to.check)
+    not.matched2 <- setdiff(names.to.check, ref.genome.names)
+    if (length(not.matched2) == 0) return(new.chr.names)
+  }
+  
   organism <- BSgenome::organism(ref.genome)
   
   if (organism == "Homo sapiens") {
     
-    # Maybe the problem is that X and Y are encoded as 23 and 24
+    # Maybe the problem is that X and Y are encoded as chr23 and chr24
     if ("chr23" %in% names.to.check) {
       new.chr.names[new.chr.names == "chr23"] <- "chrX"
       names.to.check <- setdiff(names.to.check, "chr23")
@@ -77,6 +85,33 @@ CheckAndFixChrNames <- function(vcf.df, ref.genome) {
     if ("24" %in% names.to.check) {
       new.chr.names[new.chr.names == "24"] <- "Y"
       names.to.check <- setdiff(names.to.check, "24")
+      names.to.check <- unique(c(names.to.check, "Y"))
+    }
+  }
+  
+  if (organism == "Mus musculus") {
+    
+    # Maybe the problem is that X and Y are encoded as chr20 and chr21
+    if ("chr20" %in% names.to.check) {
+      new.chr.names[new.chr.names == "chr20"] <- "chrX"
+      names.to.check <- setdiff(names.to.check, "chr20")
+      names.to.check <- unique(c(names.to.check, "chrX"))
+    }
+    if ("chr21" %in% names.to.check) {
+      new.chr.names[new.chr.names == "chr21"] <- "chrY"
+      names.to.check <- setdiff(names.to.check, "chr21")
+      names.to.check <- unique(c(names.to.check, "chrY"))
+    }
+    
+    # Maybe the problem is that X and Y are encoded as 20 and 21
+    if ("20" %in% names.to.check) {
+      new.chr.names[new.chr.names == "20"] <- "X"
+      names.to.check <- setdiff(names.to.check, "20")
+      names.to.check <- unique(c(names.to.check, "X"))
+    }
+    if ("21" %in% names.to.check) {
+      new.chr.names[new.chr.names == "21"] <- "Y"
+      names.to.check <- setdiff(names.to.check, "21")
       names.to.check <- unique(c(names.to.check, "Y"))
     }
   }
