3b88879a4339704409437870838b5082416a5dc9
nat <nattzy94@gmail.com>
2019-10-23 18:11:10 +0800

    created 2 exported functions PlotTransBiasDist2TSS() and PlotTransBiasDist2TSSToPDF()
    Includes 2 internal functions dist2TSS() and Plotdist2TSS()

--- R/dist2TSS.R
+++ R/dist2TSS.R
@@ -1,4 +1,4 @@
-dist2TSS <- function(annotated.SBS.vcf) {
+dist2TSS <- function(annotated.SBS.vcf, plot.type) {
   
   df <- annotated.SBS.vcf
   dist2TSSbin <- function(df, pos = "POS", TSS = "trans.start.pos") {
@@ -23,7 +23,24 @@ dist2TSS <- function(annotated.SBS.vcf) {
   
   df$mutation <- paste0(df$REF, ">", df$ALT)
   i <- which(df$trans.strand == "-")
-  df$mutation[i] <- paste0(ICAMS:::revc(df$REF[i]), ">", ICAMS:::revc(df$ALT[i]))
+  df$mutation[i] <- paste0(revc(df$REF[i]), ">", revc(df$ALT[i]))
+  
+  revc1 <- function(string) {
+    start <- revc(substr(string, 1, 1))
+    end <- revc(substr(string, 3, 3))
+    return(paste0(start, ">", end))
+  }
+  df <- df[df$mutation %in% c(plot.type, revc1(plot.type)), ]
+  
+  ## logistic regression. Should regress to AS/S ratio instead of trans.strand? Not working as 
+  ## this is probably regressing the wrong parameters.
+
+  type <- c('C>A', 'C>T', 'C>G', "T>A", "T>C", 'T>G')
+  df$class <- 0
+  df$class[df$mutation %in% type] <- 1
+  
+  fit<-glm(as.factor(df$class) ~ df$dist2TSS, family = 'binomial')
+  pValue <- coef(summary(fit))[2, 4]
   
   output <- matrix(data = NA, nrow = 10, ncol = 12)
   rownames(output) <- paste0("group", 1:10)
@@ -47,82 +64,103 @@ dist2TSS <- function(annotated.SBS.vcf) {
   output <- cbind(output, Sense = rowSums(output[, c(4:6, 10:12)]))
   output <- cbind(output, antiSense = rowSums(output[, c(1:3, 7:9)]))
   
-  return(output)
+  return(list(matrix = output, pvalue = pValue))
 }
 
-Plotdist2TSS <- function(output, main="") {
-  output <- data.frame(output)
-  output2 <- output[,c("antiSense","Sense")]
+Plotdist2TSS <- function(output, plot.type) {
+  output1 <- data.frame(output$matrix)
+  output2 <- output1[,c("antiSense","Sense")]
   rownames(output2) <- c("0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", 
                          "8-9", "9-10")
   colnames(output2) <- c("antiSense", "Sense")
   output2 <- t(output2)
   
-  ## logistic regression. Should regress to AS/S ratio instead of trans.strand? Not working as 
-  ## this is probably regressing the wrong parameters.
-  
-  ## df$newStrand <- NA
-  ## df$newStrand[df$trans.strand == '+'] <- 'sense'
-  ## df$newStrand[df$trans.strand == '-'] <- 'antisense'
-  ## fit<-glm(as.factor(df$newStrand) ~ df$dist2TSS, family = 'binomial')
-  ## pValue <- capture.output(cat('p =', round(coef(summary(fit))[2])))
-  
   barplot(output2, beside = TRUE, ylim = c(0, max(output2)*1.2), 
                            col = c("grey80", "grey32"), cex.axis = 0.8, cex.names = 0.9, 
-          ylab="# of mutations",main=main) 
+          ylab="# of mutations", main=plot.type) 
   legend("topright", c("antiSense", "Sense"), bty="n", 
                                         fill=c("grey80", "grey32"),cex=0.8)
-  # text (27.4, max(output2)*0.74, pValue, cex=0.8)
+  text (28.9, max(output2)*0.74, paste0('p value =', 
+                                        formatC(output$pvalue, format = "e", digits = 2)), 
+                                        cex=0.8, xpd = NA)
 }
 
-## Plotting individual mutation classes. Mutation class can be specified in this function.
-## Use PlotStrandBiasTSSAll to plot all mutations classes.
-
-PlotStrandBiasTSS <- function(matrix, class) {
-  
-  stopifnot(class %in% c("C>A", "C>G", "C>T", "T>A", "T>C", "T>G"))
-  dt <- data.table(matrix)
-  revc1 <- function(string) {
-    return(paste0(ICAMS:::revc(substr(string, 1, 1)), '>', 
-                  ICAMS:::revc(substr(string, 3, 3))))
-  }
-  output <- dt[, c(revc1(class), class), with = FALSE]
-  colnames(output) <- c("antiSense", "Sense")
-  barplot(t(output), beside=TRUE, ylim = c(0, max(output) + 10), 
-          col = c("grey80", "grey32"), cex.axis = 0.8, cex.names = 0.4, ylab="# of mutations",
-          names.arg=paste0('group', 1:10),main=paste0(class, "mutations"))
-  legend("topright", c("antiSense", "Sense"), bty="n", 
-         fill=c("grey80", "grey32"))
-  return(output)
-}
-
-PlotStrandBiasTSSAll <- function(matrix) {
-  
-  dt <- data.table(matrix)
-  revc1 <- function(string) {
-    return(paste0(ICAMS:::revc(substr(string, 1, 1)), '>', 
-                  ICAMS:::revc(substr(string, 3, 3))))
-  }
-  
-  for (class in c("C>A", "C>G", "C>T", "T>A", "T>C", "T>G")) {
-    output <- dt[, c(revc1(class), class), with = FALSE]
-    barplot(t(output), beside=TRUE, ylim = c(0, max(output) *1.2), 
-            col = c("grey80", "grey32"), cex.axis = 0.8, cex.names = 0.4, ylab="# of mutations",
-            names.arg=paste0('group', 1:10))
-    legend("topright", c("antiSense", "Sense"), bty="n", 
-           fill=c("grey80", "grey32"))
-    title(c(class, "mutations"))
-  }
-}
-
-#' Plot distance to TSS to a PDF file. Unfinished, not working yet.
+#' Plot transcriptional strand bias with respect to distance to transcription
+#' start site
+#' 
+#' @param annotated.SBS.vcf An annotated SBS VCF as a data.table which contains
+#'   sequence context and transcript information. Please refer to
+#'   \code{\link{AnnotateSBSVCF}} for more details.
 #'
-#' @param file The name of the PDF file to be produced.
+#' @param plot.type A character string indicating one mutation type to be
+#'   plotted. It should be one of "C>A", "C>G", "C>T", "T>A", "T>C", "T>G".
+#' 
+#' @importFrom stats coef
+#' 
+#' @return \code{invisible(TRUE)}
+#' 
+#' @export
+#'
+#' @examples 
+#' file <- c(system.file("extdata",
+#'                       "Strelka.SBS.GRCh37.vcf",
+#'                       package = "ICAMS"))
+#' list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs(file)
+#' SBS.vcf <- list.of.vcfs$SBS.vcfs[[1]]             
+#' if (requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5", quietly = TRUE)) {
+#'   annotated.SBS.vcf <- AnnotateSBSVCF(SBS.vcf, ref.genome = "hg19",
+#'                                       trans.ranges = trans.ranges.GRCh37)
+#'   PlotTransBiasDist2TSS(annotated.SBS.vcf, plot.type = c("C>A", "C>T"))
+#' }
+PlotTransBiasDist2TSS <- function (annotated.SBS.vcf, plot.type){
+  output <- dist2TSS(annotated.SBS.vcf, plot.type)
+  Plotdist2TSS(output, plot.type)
+  return(invisible(TRUE))
+}
 
-## dist2PDF <- function(file) {
-##   grDevices::cairo_pdf(file, width = 8.2677, height = 11.6929, onefile = TRUE)
-##   (barplot(output2, beside = TRUE, ylim = c(0, max(output) + 30), legend = TRUE))
-##   grDevices::dev.off()
-##   invisible(TRUE)
-##   
-## }
+#' Plot transcriptional strand bias with respect to distance to transcription
+#' start site to a PDF file
+#'
+#' @param annotated.SBS.vcf An annotated SBS VCF as a data.table which contains
+#'   sequence context and transcript information. Please refer to
+#'   \code{\link{AnnotateSBSVCF}} for more details.
+#'
+#' @param plot.type A vector of character indicating types to be plotted. It
+#'   should be within "C>A", "C>G", "C>T", "T>A", "T>C", "T>G".
+#'
+#' @param file The name of output file
+#'    
+#' @importFrom stats glm
+#' 
+#' @return \code{invisible(TRUE)}
+#' 
+#' @export
+#'
+#' @examples
+#' file <- c(system.file("extdata",
+#'                       "Strelka.SBS.GRCh37.vcf",
+#'                       package = "ICAMS"))
+#' list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs(file)
+#' SBS.vcf <- list.of.vcfs$SBS.vcfs[[1]]             
+#' if (requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5", quietly = TRUE)) {
+#'   annotated.SBS.vcf <- AnnotateSBSVCF(SBS.vcf, ref.genome = "hg19",
+#'                                       trans.ranges = trans.ranges.GRCh37)
+#'   PlotTransBiasDist2TSSToPDF(annotated.SBS.vcf = annotated.SBS.vcf, 
+#'                              plot.type = c("C>A","C>G","C>T","T>A","T>C"),
+#'                              file = file.path(tempdir(), "test.pdf"))
+#' }
+PlotTransBiasDist2TSSToPDF <- function(annotated.SBS.vcf, plot.type, file) {
+  grDevices::cairo_pdf(file, width = 8.2677, height = 11.6929, onefile = TRUE)
+  opar <- par(mfrow = c(4, 2), mar = c(8, 5.5, 2, 1), oma = c(1, 1, 2, 1))
+  on.exit(par(opar))
+  num <- length(plot.type)
+  type <- plot.type
+  for (i in 1:num) {
+    PlotTransBiasDist2TSS(annotated.SBS.vcf = annotated.SBS.vcf, 
+                          plot.type = plot.type[i])
+  }
+  
+  grDevices::dev.off()
+  invisible(TRUE)
+  
+}
