291cd2ae5e61f9862e25b4075aff5bb082b4c952
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-10-25 18:47:12 +0800

    Combined test functions for transcription strand bias into one file

--- tests/testthat/test_transbias_functions.R
+++ tests/testthat/test_transbias_functions.R
@@ -1,4 +1,4 @@
-context("PlotTransBiasExp and PlotTransBiasExpToPdf")
+context("Transcription strand bias functions")
 
 test_that("PlotTransBiasExp function", {
   skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
@@ -12,7 +12,6 @@ test_that("PlotTransBiasExp function", {
                      Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
                      num.of.bins = 4, plot.type = "C>A")
   expect_equal(out, TRUE)
-  
 })
 
 test_that("PlotTransBiasExpToPdf function", {
@@ -23,11 +22,34 @@ test_that("PlotTransBiasExpToPdf function", {
     AnnotateSBSVCF(list.of.vcfs$SBS.vcfs[[1]], "hg19", trans.ranges.GRCh37)
   out <- 
     PlotTransBiasExpToPdf(annotated.SBS.vcf = annotated.SBS.vcf,
+                          file = file.path(tempdir(), "test.pdf"),
                           expression.level = gene.expression.level.example.GRCh37, 
                           Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
-                          num.of.bins = 4, 
-                          plot.type = c("C>A","C>G","C>T","T>A","T>C"), 
-                          file = file.path(tempdir(), "test.pdf"))
+                          num.of.bins = 4
+                          )
+  expect_equal(out, TRUE)
+  
+})
+
+test_that("PlotTransBiasDist2TSS function", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
+  annotated.SBS.vcf <- 
+    AnnotateSBSVCF(list.of.vcfs$SBS.vcfs[[1]], "hg19", trans.ranges.GRCh37)
+  out <- PlotTransBiasDist2TSS(annotated.SBS.vcf, "C>A")
+  expect_equal(out, TRUE)
+  
+})
+
+test_that("PlotTransBiasDist2TSSToPDF function", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
+  annotated.SBS.vcf <- 
+    AnnotateSBSVCF(list.of.vcfs$SBS.vcfs[[1]], "hg19", trans.ranges.GRCh37)
+  out <- PlotTransBiasDist2TSSToPDF(annotated.SBS.vcf, 
+                                    file = file.path(tempdir(), "test.pdf"))
   expect_equal(out, TRUE)
   
 })
