d00f46070b389fa0cb7c50d1b5f298dc5cb98186
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-10-25 08:29:51 +0800

    Create test.PlotTransBiasExp.R

---
+++ tests/testthat/test.PlotTransBiasExp.R
@@ -0,0 +1,32 @@
+context("PlotTransBiasExp and PlotTransBiasExpToPdf")
+
+test_that("PlotTransBiasExp function", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
+  annotated.SBS.vcf <- 
+    AnnotateSBSVCF(list.of.vcfs$SBS.vcfs[[1]], "hg19", trans.ranges.GRCh37)
+  out <- 
+    PlotTransBiasExp(annotated.SBS.vcf = annotated.SBS.vcf, 
+                     expression.level = gene.expression.level.example.GRCh37, 
+                     Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
+                     num.of.bins = 4, plot.type = "C>A")
+  expect_equal(out, TRUE)
+  
+})
+
+test_that("PlotTransBiasExpToPdf function", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
+  annotated.SBS.vcf <- 
+    AnnotateSBSVCF(list.of.vcfs$SBS.vcfs[[1]], "hg19", trans.ranges.GRCh37)
+  out <- 
+    PlotTransBiasExpToPdf(annotated.SBS.vcf = annotated.SBS.vcf,                          expression.level = gene.expression.level.example.GRCh37, 
+                          Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
+                          num.of.bins = 4, 
+                          plot.type = c("C>A","C>G","C>T","T>A","T>C"), 
+                          file = file.path(tempdir(), "test.pdf"))
+  expect_equal(out, TRUE)
+  
+})
