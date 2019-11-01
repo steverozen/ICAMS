928a3f11eb522038fc67f1262ed10af94246eaab
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-11-01 18:52:53 +0800

    Update test_strandbias_functions.R

--- tests/testthat/test_strandbias_functions.R
+++ tests/testthat/test_strandbias_functions.R
@@ -6,30 +6,59 @@ test_that("PlotTransBiasGeneExpToPdf function", {
   list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
   annotated.SBS.vcf <- 
     AnnotateSBSVCF(list.of.vcfs$SBS.vcfs[[1]], "hg19", trans.ranges.GRCh37)
+  vcf1 <- annotated.SBS.vcf[REF == "C",]
+  vcf2 <- annotated.SBS.vcf[REF %in% c("C", "G"), ]
+  vcf3 <- annotated.SBS.vcf[REF == "T",]
+  vcf4 <- annotated.SBS.vcf[REF %in% c("A", "T"), ]
   out <- 
     PlotTransBiasGeneExpToPdf(annotated.SBS.vcf = annotated.SBS.vcf,
-                          file = file.path(tempdir(), "test1.pdf"),
-                          expression.level = gene.expression.level.example.GRCh37, 
-                          Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
-                          num.of.bins = 4
-                          )
+                              file = file.path(tempdir(), "test.pdf"),
+                              expression.level = gene.expression.level.example.GRCh37, 
+                              Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
+                              num.of.bins = 4
+    )
+  out1 <- 
+    PlotTransBiasGeneExpToPdf(annotated.SBS.vcf = vcf1,
+                              file = file.path(tempdir(), "test1.pdf"),
+                              expression.level = gene.expression.level.example.GRCh37, 
+                              Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
+                              num.of.bins = 4
+    )
+  out2 <- 
+    PlotTransBiasGeneExpToPdf(annotated.SBS.vcf = vcf2,
+                              file = file.path(tempdir(), "test2.pdf"),
+                              expression.level = gene.expression.level.example.GRCh37, 
+                              Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
+                              num.of.bins = 4
+    )
+  out3 <- 
+    PlotTransBiasGeneExpToPdf(annotated.SBS.vcf = vcf3,
+                              file = file.path(tempdir(), "test3.pdf"),
+                              expression.level = gene.expression.level.example.GRCh37, 
+                              Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
+                              num.of.bins = 4
+    )
+  out4 <- 
+    PlotTransBiasGeneExpToPdf(annotated.SBS.vcf = vcf4,
+                              file = file.path(tempdir(), "test4.pdf"),
+                              expression.level = gene.expression.level.example.GRCh37, 
+                              Ensembl.gene.ID.col = "Ensembl.gene.ID", TPM.col = "TPM",
+                              num.of.bins = 4
+    )
+  
+  
   expect_equal(out, TRUE)
+  expect_equal(out1, TRUE)
+  expect_equal(out2, TRUE)
+  expect_equal(out3, TRUE)
+  expect_equal(out4, TRUE)
+  unlink(file.path(tempdir(), "test.pdf"))
   unlink(file.path(tempdir(), "test1.pdf"))
+  unlink(file.path(tempdir(), "test2.pdf"))
+  unlink(file.path(tempdir(), "test3.pdf"))
+  unlink(file.path(tempdir(), "test4.pdf"))
 })
 
-test_that("PlotTransBiasDist2TSSToPdf function", {
-  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
-  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
-  list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
-  annotated.SBS.vcf <- 
-    AnnotateSBSVCF(list.of.vcfs$SBS.vcfs[[1]], "hg19", trans.ranges.GRCh37)
-  out <- PlotTransBiasDist2TSSToPdf(annotated.SBS.vcf, 
-                                    plot.type = c("C>A", "C>G"),
-                                    file = file.path(tempdir(), "test2.pdf"))
-  expect_equal(out, TRUE)
-  unlink(file.path(tempdir(), "test2.pdf"))
-  
-})
 
 test_that("PlotTransBiasGeneExp function", {
   skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
@@ -77,13 +106,62 @@ test_that("PlotTransBiasGeneExp function", {
   expect_equal(out4, TRUE)
 })
 
-test_that("PlotTransBiasDist2TSS function", {
+test_that("PlotTransBiasDistToTSSToPdf function", {
   skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
   stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
   list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
   annotated.SBS.vcf <- 
     AnnotateSBSVCF(list.of.vcfs$SBS.vcfs[[1]], "hg19", trans.ranges.GRCh37)
-  out <- PlotTransBiasDist2TSS(annotated.SBS.vcf, "C>A")
+  vcf1 <- annotated.SBS.vcf[REF == "C",]
+  vcf2 <- annotated.SBS.vcf[REF %in% c("C", "G"), ]
+  vcf3 <- annotated.SBS.vcf[REF == "T",]
+  vcf4 <- annotated.SBS.vcf[REF %in% c("A", "T"), ]
+  out <- PlotTransBiasDistToTSSToPdf(annotated.SBS.vcf, 
+                                     file = file.path(tempdir(), "test.pdf"))
+  out1 <- PlotTransBiasDistToTSSToPdf(vcf1, 
+                                      file = file.path(tempdir(), "test1.pdf"))
+  out2 <- PlotTransBiasDistToTSSToPdf(vcf2, 
+                                      file = file.path(tempdir(), "test2.pdf"))
+  out3 <- PlotTransBiasDistToTSSToPdf(vcf3, 
+                                      file = file.path(tempdir(), "test3.pdf"))
+  out4 <- PlotTransBiasDistToTSSToPdf(vcf4, 
+                                      file = file.path(tempdir(), "test4.pdf"))
   expect_equal(out, TRUE)
+  expect_equal(out1, TRUE)
+  expect_equal(out2, TRUE)
+  expect_equal(out3, TRUE)
+  expect_equal(out4, TRUE)
+  unlink(file.path(tempdir(), "test.pdf"))
+  unlink(file.path(tempdir(), "test1.pdf"))
+  unlink(file.path(tempdir(), "test2.pdf"))
+  unlink(file.path(tempdir(), "test3.pdf"))
+  unlink(file.path(tempdir(), "test4.pdf"))
+})
+
+test_that("PlotTransBiasDistToTSS function", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  list.of.vcfs <- ReadAndSplitStrelkaSBSVCFs("testdata/Strelka.SBS.GRCh37.vcf")
+  annotated.SBS.vcf <- 
+    AnnotateSBSVCF(list.of.vcfs$SBS.vcfs[[1]], "hg19", trans.ranges.GRCh37)
+  vcf1 <- annotated.SBS.vcf[REF == "C",]
+  vcf2 <- annotated.SBS.vcf[REF %in% c("C", "G"), ]
+  vcf3 <- annotated.SBS.vcf[REF == "T",]
+  vcf4 <- annotated.SBS.vcf[REF %in% c("A", "T"), ]
+  out <- PlotTransBiasDistToTSS(annotated.SBS.vcf = annotated.SBS.vcf,
+                               plot.type = "C>A")
+  out1 <- PlotTransBiasDistToTSS(annotated.SBS.vcf = vcf1,
+                               plot.type = "C>A")
+  out2 <- PlotTransBiasDistToTSS(annotated.SBS.vcf = vcf2,
+                                plot.type = "C>A")
+  out3 <- PlotTransBiasDistToTSS(annotated.SBS.vcf = vcf3,
+                                plot.type = "C>A")
+  out4 <- PlotTransBiasDistToTSS(annotated.SBS.vcf = vcf4,
+                                plot.type = "C>A")
+  expect_equal(out, TRUE)
+  expect_equal(out1, TRUE)
+  expect_equal(out2, TRUE)
+  expect_equal(out3, TRUE)
+  expect_equal(out4, TRUE)
   unlink("Rplots.pdf")
 })
