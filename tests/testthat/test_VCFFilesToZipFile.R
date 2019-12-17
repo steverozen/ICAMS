e43637e63443586de1a9ead33c9e759b1687d0cd
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-12-17 10:45:01 +0800

    Created automated test for function StrelkaIDVCFFilesToZipFile()

--- tests/testthat/test_VCFFilesToZipFile.R
+++ tests/testthat/test_VCFFilesToZipFile.R
@@ -31,3 +31,18 @@ test_that("StrelkaSBSVCFFilesToZipFile function", {
   graphics.off()
   unlink("testdata/Strelka-SBS-GRCh37/Rplots.pdf")
 })
+
+test_that("StrelkaIDVCFFilesToZipFile function", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  file <- "testdata/Strelka-ID-GRCh37/"
+  out <- StrelkaIDVCFFilesToZipFile(file, ref.genome = "hg19",
+                                    region = "genome",
+                                    zipfile.name = "test")
+  expect_type(out, "list")
+  name <- grep(".zip", list.files("testdata/Strelka-ID-GRCh37/"), value = TRUE)
+  expect_equal(name, "test.zip")
+  unlink("testdata/Strelka-ID-GRCh37/test.zip")
+  graphics.off()
+  unlink("testdata/Strelka-ID-GRCh37/Rplots.pdf")
+})
