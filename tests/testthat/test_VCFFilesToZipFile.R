a1fe7381e72938613e84ebaa6370699d758a5086
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-12-23 14:39:44 +0800

    Update test_VCFFilesToZipFile.R

--- tests/testthat/test_VCFFilesToZipFile.R
+++ tests/testthat/test_VCFFilesToZipFile.R
@@ -3,15 +3,17 @@ context("VCFFilestoZipfile functions")
 test_that("MutectVCFFilesToZipFile function", {
   skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
   stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
-  file <- "testdata/Mutect-GRCh37/"
-  out <- MutectVCFFilesToZipFile(file, ref.genome = "hg19",
+  dir <- "testdata/Mutect-GRCh37"
+  out <- MutectVCFFilesToZipFile(dir, 
+                                 file = tempdir(), 
+                                 ref.genome = "hg19",
                                  trans.ranges = trans.ranges.GRCh37,
                                  region = "genome",
                                  zipfile.name = "test")
   expect_type(out, "list")
-  name <- grep(".zip", list.files("testdata/Mutect-GRCh37/"), value = TRUE)
+  name <- grep(".zip", list.files(tempdir()), value = TRUE)
   expect_equal(name, "test.zip")
-  unlink("testdata/Mutect-GRCh37/test.zip")
+  unlink(paste0(tempdir(), "/test.zip"))
   graphics.off()
   unlink("testdata/Mutect-GRCh37/Rplots.pdf")
 })
@@ -19,15 +21,17 @@ test_that("MutectVCFFilesToZipFile function", {
 test_that("StrelkaSBSVCFFilesToZipFile function", {
   skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
   stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
-  file <- "testdata/Strelka-SBS-GRCh37/"
-  out <- StrelkaSBSVCFFilesToZipFile(file, ref.genome = "hg19",
+  dir <- "testdata/Strelka-SBS-GRCh37"
+  out <- StrelkaSBSVCFFilesToZipFile(dir,
+                                     file = tempdir(), 
+                                     ref.genome = "hg19",
                                      trans.ranges = trans.ranges.GRCh37,
                                      region = "genome",
                                      zipfile.name = "test")
   expect_type(out, "list")
-  name <- grep(".zip", list.files("testdata/Strelka-SBS-GRCh37/"), value = TRUE)
+  name <- grep(".zip", list.files(tempdir()), value = TRUE)
   expect_equal(name, "test.zip")
-  unlink("testdata/Strelka-SBS-GRCh37/test.zip")
+  unlink(paste0(tempdir(), "/test.zip"))
   graphics.off()
   unlink("testdata/Strelka-SBS-GRCh37/Rplots.pdf")
 })
@@ -35,14 +39,16 @@ test_that("StrelkaSBSVCFFilesToZipFile function", {
 test_that("StrelkaIDVCFFilesToZipFile function", {
   skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
   stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
-  file <- "testdata/Strelka-ID-GRCh37/"
-  out <- StrelkaIDVCFFilesToZipFile(file, ref.genome = "hg19",
+  dir <- "testdata/Strelka-ID-GRCh37"
+  out <- StrelkaIDVCFFilesToZipFile(dir,
+                                    file = tempdir(),
+                                    ref.genome = "hg19",
                                     region = "genome",
                                     zipfile.name = "test")
   expect_type(out, "list")
-  name <- grep(".zip", list.files("testdata/Strelka-ID-GRCh37/"), value = TRUE)
+  name <- grep(".zip", list.files(tempdir()), value = TRUE)
   expect_equal(name, "test.zip")
-  unlink("testdata/Strelka-ID-GRCh37/test.zip")
+  unlink(paste0(tempdir(), "/test.zip"))
   graphics.off()
   unlink("testdata/Strelka-ID-GRCh37/Rplots.pdf")
 })
