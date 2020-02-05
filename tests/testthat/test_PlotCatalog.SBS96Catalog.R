76a34aadf4b5fef8ef836901e7024b322821bee9
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-02-05 21:44:27 +0800

    Added tests for setting xlabels = FALSE for function PlotCatalog.SBS96Catalog()

--- tests/testthat/test_PlotCatalog.SBS96Catalog.R
+++ tests/testthat/test_PlotCatalog.SBS96Catalog.R
@@ -12,10 +12,20 @@ test_that("PlotCatalog.SBS96Catalog for one column catalog", {
   out <- PlotCatalog(cat.counts)
   out1 <- PlotCatalog(cat.counts, ylim = c(0, 1500))
   out2 <- PlotCatalog(cat.counts, ylim = c(0, 1500), cex = 0.8)
+  out3 <- PlotCatalog(cat.counts, ylim = c(0, 1500), cex = 0.8, 
+                      xlabels = FALSE)
+  par(tck = 0)
+  out4 <- PlotCatalog(cat.counts, ylim = c(0, 1500), cex = 0.8, 
+                      xlabels = FALSE)
   expect_equal(out$plot.success, TRUE)
   expect_equal(out1$plot.success, TRUE)
   expect_equal(out2$plot.success, TRUE)
-
+  expect_equal(out3$plot.success, TRUE)
+  expect_equal(out4$plot.success, TRUE)
+  graphics.off()
+  
+  opar <- par(mar = c(5.5, 6, 5, 1))
+  on.exit(par(opar))
   cat.density <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
@@ -23,10 +33,20 @@ test_that("PlotCatalog.SBS96Catalog for one column catalog", {
   out <- PlotCatalog(cat.density)
   out1 <- PlotCatalog(cat.density, ylim = c(0, 20))
   out2 <- PlotCatalog(cat.density, ylim = c(0, 20), cex = 0.8)
+  out3 <- PlotCatalog(cat.density, ylim = c(0, 20), cex = 0.8,
+                      xlabels = FALSE)
+  par(tck = 0)
+  out4 <- PlotCatalog(cat.density, ylim = c(0, 20), cex = 0.8, 
+                      xlabels = FALSE)
   expect_equal(out$plot.success, TRUE)
   expect_equal(out1$plot.success, TRUE)
   expect_equal(out2$plot.success, TRUE)
-
+  expect_equal(out3$plot.success, TRUE)
+  expect_equal(out4$plot.success, TRUE)
+  graphics.off()
+  
+  opar <- par(mar = c(5.5, 6, 5, 1))
+  on.exit(par(opar))
   cat.counts.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
@@ -34,20 +54,37 @@ test_that("PlotCatalog.SBS96Catalog for one column catalog", {
   out <- PlotCatalog(cat.counts.signature)
   out1 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.2))
   out2 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.2), cex = 0.8)
+  out3 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.2), cex = 0.8,
+                      xlabels = FALSE)
+  par(tck = 0)
+  out4 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.2), cex = 0.8,
+                      xlabels = FALSE)
   expect_equal(out$plot.success, TRUE)
   expect_equal(out1$plot.success, TRUE)
   expect_equal(out2$plot.success, TRUE)
-
+  expect_equal(out3$plot.success, TRUE)
+  expect_equal(out4$plot.success, TRUE)
+  graphics.off()
+  
+  opar <- par(mar = c(5.5, 6, 5, 1))
+  on.exit(par(opar))
   cat.density.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "density.signature")
   out <- PlotCatalog(cat.density.signature)
   out1 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.2))
-  out1 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.2), cex = 0.8)
+  out2 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.2), cex = 0.8)
+  out3 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.2), cex = 0.8,
+                      xlabels = FALSE)
+  par(tck = 0)
+  out4 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.2), cex = 0.8,
+                      xlabels = FALSE)
   expect_equal(out$plot.success, TRUE)
   expect_equal(out1$plot.success, TRUE)
   expect_equal(out2$plot.success, TRUE)
+  expect_equal(out3$plot.success, TRUE)
+  expect_equal(out4$plot.success, TRUE)
   graphics.off()
 })
 
@@ -63,41 +100,78 @@ test_that("PlotCatalog.SBS96Catalog for two column catalog", {
   out <- PlotCatalog(cat.counts)
   out1 <- PlotCatalog(cat.counts, ylim = c(0, 2500))
   out2 <- PlotCatalog(cat.counts, ylim = c(0, 2500), cex = 0.8)
+  out3 <- PlotCatalog(cat.counts, ylim = c(0, 2500), cex = 0.8, 
+                      xlabels = FALSE)
+  par(tck = 0)
+  out4 <- PlotCatalog(cat.counts, ylim = c(0, 2500), cex = 0.8, 
+                      xlabels = FALSE)
   expect_equal(out$plot.success, TRUE)
   expect_equal(out1$plot.success, TRUE)
   expect_equal(out2$plot.success, TRUE)
+  expect_equal(out3$plot.success, TRUE)
+  expect_equal(out4$plot.success, TRUE)
+  graphics.off()
   
+  opar <- par(mar = c(5.5, 6, 5, 1))
+  on.exit(par(opar))
   cat.density <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "density")
   out <- PlotCatalog(cat.density)
   out1 <- PlotCatalog(cat.density, ylim = c(0, 35))
-  out1 <- PlotCatalog(cat.density, ylim = c(0, 35), cex = 0.8)
+  out2 <- PlotCatalog(cat.density, ylim = c(0, 35), cex = 0.8)
+  out3 <- PlotCatalog(cat.density, ylim = c(0, 35), cex = 0.8,
+                      xlabels = FALSE)
+  par(tck = 0)
+  out4 <- PlotCatalog(cat.density, ylim = c(0, 35), cex = 0.8, 
+                      xlabels = FALSE)
   expect_equal(out$plot.success, TRUE)
   expect_equal(out1$plot.success, TRUE)
   expect_equal(out2$plot.success, TRUE)
+  expect_equal(out3$plot.success, TRUE)
+  expect_equal(out4$plot.success, TRUE)
+  graphics.off()
   
+  opar <- par(mar = c(5.5, 6, 5, 1))
+  on.exit(par(opar))
   cat.counts.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "counts.signature")
   out <- PlotCatalog(cat.counts.signature)
   out1 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.3))
-  out1 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.3), cex = 0.8)
+  out2 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.3), cex = 0.8)
+  out3 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.3), cex = 0.8,
+                      xlabels = FALSE)
+  par(tck = 0)
+  out4 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.3), cex = 0.8,
+                      xlabels = FALSE)
   expect_equal(out$plot.success, TRUE)
   expect_equal(out1$plot.success, TRUE)
   expect_equal(out2$plot.success, TRUE)
+  expect_equal(out3$plot.success, TRUE)
+  expect_equal(out4$plot.success, TRUE)
+  graphics.off()
   
+  opar <- par(mar = c(5.5, 6, 5, 1))
+  on.exit(par(opar))
   cat.density.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "density.signature")
   out <- PlotCatalog(cat.density.signature)
   out1 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.3))
-  out1 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.3), cex = 0.8)
+  out2 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.3), cex = 0.8)
+  out3 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.3), cex = 0.8,
+                      xlabels = FALSE)
+  par(tck = 0)
+  out4 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.3), cex = 0.8,
+                      xlabels = FALSE)
   expect_equal(out$plot.success, TRUE)
   expect_equal(out1$plot.success, TRUE)
   expect_equal(out2$plot.success, TRUE)
+  expect_equal(out3$plot.success, TRUE)
+  expect_equal(out4$plot.success, TRUE)
   graphics.off()
 })
