12c58f7723fe4b3c17f55cbb92855a4ef1c4cc96
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-02-05 20:13:48 +0800

    Added tests for setting ylim for function PlotCatalog.SBS96Catalog()

--- tests/testthat/test_PlotCatalog.SBS96Catalog.R
+++ tests/testthat/test_PlotCatalog.SBS96Catalog.R
@@ -10,28 +10,36 @@ test_that("PlotCatalog.SBS96Catalog for one column catalog", {
                 region = "genome", catalog.type = "counts")
   cat.counts <- catalog.counts[, 1, drop = FALSE]
   out <- PlotCatalog(cat.counts)
+  out1 <- PlotCatalog(cat.counts, ylim = c(0, 1500))
   expect_equal(out$plot.success, TRUE)
+  expect_equal(out1$plot.success, TRUE)
 
   cat.density <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "density")
   out <- PlotCatalog(cat.density)
+  out1 <- PlotCatalog(cat.density, ylim = c(0, 20))
   expect_equal(out$plot.success, TRUE)
+  expect_equal(out1$plot.success, TRUE)
 
   cat.counts.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "counts.signature")
   out <- PlotCatalog(cat.counts.signature)
+  out1 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.2))
   expect_equal(out$plot.success, TRUE)
+  expect_equal(out1$plot.success, TRUE)
 
   cat.density.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "density.signature")
   out <- PlotCatalog(cat.density.signature)
+  out1 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.2))
   expect_equal(out$plot.success, TRUE)
+  expect_equal(out1$plot.success, TRUE)
   graphics.off()
 })
 
@@ -45,27 +53,35 @@ test_that("PlotCatalog.SBS96Catalog for two column catalog", {
                 region = "genome", catalog.type = "counts")
   cat.counts <- catalog.counts[, 1:2]
   out <- PlotCatalog(cat.counts)
+  out1 <- PlotCatalog(cat.counts, ylim = c(0, 2500))
   expect_equal(out$plot.success, TRUE)
+  expect_equal(out1$plot.success, TRUE)
   
   cat.density <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "density")
   out <- PlotCatalog(cat.density)
+  out1 <- PlotCatalog(cat.density, ylim = c(0, 35))
   expect_equal(out$plot.success, TRUE)
+  expect_equal(out1$plot.success, TRUE)
   
   cat.counts.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "counts.signature")
   out <- PlotCatalog(cat.counts.signature)
+  out1 <- PlotCatalog(cat.counts.signature, ylim = c(0, 0.3))
   expect_equal(out$plot.success, TRUE)
+  expect_equal(out1$plot.success, TRUE)
   
   cat.density.signature <-
     TransformCatalog(cat.counts, target.ref.genome = "GRCh37",
                      target.region = "genome",
                      target.catalog.type = "density.signature")
   out <- PlotCatalog(cat.density.signature)
+  out1 <- PlotCatalog(cat.density.signature, ylim = c(0, 0.3))
   expect_equal(out$plot.success, TRUE)
+  expect_equal(out1$plot.success, TRUE)
   graphics.off()
 })
