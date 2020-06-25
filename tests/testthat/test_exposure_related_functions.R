d8387e109069e230a1e3073042a509c56ad0c93a
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-06-25 19:23:42 +0800

    Added automated tests for reading and writing exposure

---
+++ tests/testthat/test_exposure_related_functions.R
@@ -0,0 +1,18 @@
+context("Exposure related functions")
+
+test_that("Read and write exposure", {
+  file <- "testdata/tiny.exposure.csv"
+  expect_warning(x <- ReadExposure(file))
+  tfile <- tempfile()
+  WriteExposure(x, tfile)
+  reread.x <- ReadExposure(tfile)
+  expect_equal(x, reread.x)
+  
+  x <- ReadExposure(file, check.names = FALSE)
+  WriteExposure(x, tfile)
+  x2 <- ReadExposure(tfile, check.names = FALSE)
+  expect_equal(x, x2)
+  
+  file2 <- "testdata/tiny.exposure.dup.csv"
+  expect_error(x <- ReadExposure(file2, check = FALSE))
+})
