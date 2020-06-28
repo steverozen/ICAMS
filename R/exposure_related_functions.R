5927565ff5602b164f57f6ac06703c823b5c4d94
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-06-28 21:00:20 +0800

    Added new arguments to exposure plotting functions
    Fixed the bug that ylab is getting lost

--- R/exposure_related_functions.R
+++ R/exposure_related_functions.R
@@ -79,76 +79,99 @@ WriteExposure <- function(exposure, file) {
 #' exposure <- ReadExposure(file)
 #' exposure.sorted <- SortExposure(exposure)
 SortExposure <- function(exposure, decreasing = TRUE) {
-  retval <- exposure[, order(colSums(exposure), decreasing = decreasing)]
+  retval <- exposure[, order(colSums(exposure), decreasing = decreasing),
+                     drop = FALSE]
   return(retval)
 }
 
 #' Plot a single exposure plot
 #'
-#' @param exposure Exposures as a numerical matrix (or data.frame) with
-#'   signatures in rows and samples in columns. Rownames are taken as the
+#' @param exposure Exposures as a numerical \code{matrix} (or \code{data.frame})
+#'   with signatures in rows and samples in columns. Rownames are taken as the
 #'   signature names and column names are taken as the sample IDs. If you want
-#'   \code{exp} sorted from largest to smallest, use \code{\link{SortExposure}}.
-#'   Do not use column names that start with multiple underscores. The exposures
-#'   will often be mutation counts, but could also be e.g. mutations per
-#'   megabase.
+#'   \code{exposure} sorted from largest to smallest, use
+#'   \code{\link{SortExposure}}. Do not use column names that start with
+#'   multiple underscores. The exposures will often be mutation counts, but
+#'   could also be e.g. mutations per megabase.
 #'
 #' @param plot.proportion Plot exposure proportions rather than counts.
 #'
-#' @param plot.legend If \code{TRUE}, plot a legend.
-#' 
+#' @param xlim,ylim Limits for the x and y axis. If \code{NULL}(default), the
+#'   function tries to do something reasonable.
+#'
+#' @param legend.x,legend.y The x and y co-ordinates to be used to position the
+#'   legend.
+#'   
 #' @param cex.legend A numerical value giving the amount by which legend
 #'   plotting text and symbols should be magnified relative to the default.
 #'   
-#' @param ... Parameters passed to \code{\link[graphics]{barplot}}.
-#'
+#' @param ... Other arguments passed to \code{\link[graphics]{barplot}}. If
+#'   \code{ylab} is not included, it defaults to a value depending on
+#'   \code{plot.proportion}. If \code{col} is not supplied the function tries to
+#'   do something reasonable.
+#'   
 #' @import graphics 
-#' 
-#' @return An invisible numeric vector giving the coordinates of all the bar
-#'   midpoints drawn, useful for adding to the graph.
+#'
+#' @return An \strong{invisible} list whose first element is a logic value
+#'   indicating whether the plot is successful. The second element is a numeric
+#'   vector giving the coordinates of all the bar midpoints drawn, useful for
+#'   adding to the graph.
 #'
 #' @keywords internal
 PlotExposureInternal <-
   function(exposure, # This is actually the exposure "counts"
            plot.proportion = FALSE,
-           plot.legend     = TRUE,
+           xlim            = NULL,
+           ylim            = NULL,
+           legend.x        = NULL,
+           legend.y        = NULL,
            cex.legend      = 0.9,
            ...
   ) {
-    exposure <- as.matrix(exposure) # in case it is a data frame
+    exposure <- as.matrix(exposure) # In case it is a data frame
     num.sigs  <- nrow(exposure)
     num.samples <- ncol(exposure)
+    args <- list(...)
     
-    three.dots <- list(...)
-    if (is.null(three.dots$col)) {
+    ylab <- args$ylab
+    if (is.null(ylab)) {
+      ylab <- ifelse(plot.proportion,
+                     "Proportion of mutations",
+                     "Number of mutations")
+    }
+    
+    if (is.null(args$col)) {
       if (num.sigs <= 8) {
-        three.dots$col <- 
+        args$col <- 
           c("red", "black", "grey", "yellow", "blue", "brown", "green4", "skyblue")
       } else {
-        # Lots of signatures; use shaded lines to differentiate
-        three.dots$col <- grDevices::rainbow(num.sigs, alpha = 1)
+        # Lots of signatures; use shading lines to differentiate
+        args$col <- grDevices::rainbow(num.sigs, alpha = 1)
       }
     }
+    
     if (num.sigs <= 12) {
-      p.dense = -1 # will repeat as needed, -1 = solid
-      p.angle = 0  # ditto
+      # Specify the density of shading lines, in lines per inch, for the bars or
+      # bar components. Will repeat as needed, non-positive values of density
+      # inhibit the drawing of shading lines.
+      density <- -1 
+      # Sepcify the slope of shading lines, given as an angle in degrees
+      # (counter-clockwise), for the bars or bar components. Will repeat as
+      # needed.
+      angle <- 0  
     } else {
-      # Lots of signatures; use shaded lines to differentiate
-      p.dense = c(-1, 50, 50, 50, 50) # will repeat as needed, -1 = solid
-      p.angle = c(0, 45, 135, 45, 135)  # ditto
+      # Lots of signatures; use shading lines to differentiate
+      density <- c(-1, 50, 50, 50, 50) # Will repeat as needed
+      angle <- c(0, 45, 135, 45, 135)  # Ditto
     }
-    # For legend, we need to put in reverse order. make sure this matches
-    # order used in barplot
-    num.repeats = ceiling(num.sigs/length(p.dense))
-    p.dense.rev = rev(rep(p.dense, num.repeats)[1:num.sigs])
-    p.angle.rev = rev(rep(p.angle, num.repeats)[1:num.sigs])
-    
-    
-    # l.cex = if (num.sigs > 15) 1.2 else 1 # char expansion for legend (was 0.7)
-    # direction = 2 # 1 = always horizontal, 2 = perpendicular to axis
+    # For legend, we need to put density and angle in reversed order to make
+    # sure this matches order used in barplot.
+    num.repeats <- ceiling(num.sigs / length(density))
+    density.rev <- rev(rep(density, num.repeats)[1:num.sigs])
+    angle.rev <- rev(rep(angle, num.repeats)[1:num.sigs])
     
     if (plot.proportion) {
-      # Matrix divided by vector goes col-wise, not row-wise, so transpose twice
+      # Matrix divided by vector goes column-wise, not row-wise, so transpose twice
       plot.what <- t(t(exposure)/colSums(exposure))
       
       # If some columns in exposure matrix are "padded", i.e. all the values
@@ -159,101 +182,113 @@ PlotExposureInternal <-
       plot.what <- exposure
     }
     
-    # plot.what <-
-    #  cbind(plot.what,
-    #        matrix(0,
-    #               nrow = nrow(plot.what),
-    #               ncol = round(ncol(plot.what) * 0.15)))
-    
-    # Ignore column names; we'll plot them separately to make them fit
-    bp = do.call(
-      barplot,
-      args = c(list(height   = plot.what,
-                    las      = 1,
-                    #yaxt     = "s",
-                    xaxt     = "n", # Do not plot the X axis
-                    density  = p.dense,
-                    angle    = p.angle,
-                    border   = "white", # ifelse(num.samples>200,NA,1),
-                    #cex.main = 1.2,
-                    xlim     = c(0, round(ncol(plot.what) * 1.25)),
-                    ylim     = c(0, max(colSums(plot.what)) * 1.1)),
-               three.dots))
-    
-    # Get max y values for plot region, put legend at top right
-    #dims = par("usr") # c(x.min, x.max, y.min, y.max)
-    #y.max = dims[4]
-    
-    if (plot.legend) {
-      # Less space between rows (y.intersp), and between box & label (x.intersp)
-      # reverse the order, so sig 1 is at bottom (to match the stacked bar graph)
-      #legend.x <- dims[2] * 0.95 #ncol(exposure) * 0.7   # Nanhai, we could pass in legend.x and legend.y as optional arguments from the caller
-      #legend.y <- y.max
-      legend(#x         = legend.x,
-        #y         = legend.y,
-        "right",
-        inset     = -0.08,
-        legend    = rev(row.names(exposure)),
-        density   = p.dense.rev,
-        angle     = p.angle.rev,
-        #bg        = NA,
-        xpd       = NA,
-        fill      = rev(three.dots$col[1:num.sigs]),
-        x.intersp = 0.3, #0.4,
-        y.intersp = 1, #0.8,
-        bty       = "n",
-        border    = "white",
-        cex       = cex.legend,
-        title     = "Signature")
-      #text(x= legend.x, y = legend.y, labels = "Signature", 
-      #xpd = NA, adj = -0.09)#-0.09)
+    # Set the limits for x axis, leave extra space for plotting legend
+    if (is.null(xlim)) {
+      xmax <- round(ncol(plot.what) * 1.7)
+      xlim <- c(0, xmax)
+    } else {
+      xmax <- xlim[2]
     }
     
-    # Now add sample names, rotated to hopefully fit
+    if (is.null(ylim)) {
+      ymax <- round(max(colSums(plot.what))) 
+      ylim <- c(0, ymax)
+    } else {
+      ymax <- ylim[2]
+    }
+    
+    # Ignore column names, we'll plot them separately to make them fit.
+    mp <- do.call(
+      barplot,
+      args = c(list(height   = plot.what,
+                    ylab     = ylab,
+                    # The amount of space left before each bar
+                    space    = xmax * 0.01, 
+                    xaxs     = "i", # No extra spacing at each end of x axis
+                    xaxt     = "n", # Do not plot the X axis
+                    yaxt     = "n", # Do not plot the Y axis
+                    density  = density,
+                    angle    = angle,
+                    border   = "white", 
+                    xlim     = xlim,
+                    ylim     = ylim),
+               args))
+    
+    # Get locations for y axis annotations
+    y.axis.values <- seq(0, ymax, ymax/4)
+    if (plot.proportion) {
+      y.axis.labels <- format(round(y.axis.values, 2), nsmall = 2)
+    } else {
+      y.axis.labels <- round(y.axis.values)
+    }
+    
+    # Draw y axis and labels
+    Axis(side = 2, at = y.axis.values, las = 1, labels = FALSE)
+    text(x = -xmax* 0.03, y = y.axis.values, labels = y.axis.labels, 
+         las = 1, adj = 1, xpd = NA)
+    
+    # Setting the default parameters for legend plotting
+    if (is.null(legend.x)) {
+      legend.x <- xmax * 0.9
+    }
+    if (is.null(legend.y)) {
+      legend.y <- ymax 
+    }
+    
+    legend(x         = legend.x,
+           y         = legend.y,
+           legend    = rev(row.names(exposure)),
+           density   = density.rev,
+           angle     = angle.rev,
+           xpd       = NA,
+           fill      = rev(args$col),
+           x.intersp = 0.3, 
+           y.intersp = 1, 
+           bty       = "n",
+           border    = "white",
+           cex       = cex.legend,
+           title     = "Signature")
+    
+    # Now add sample names, rotated to hopefully fit,
     # don't even try to show all if there are too many
     if (num.samples <= 200) {
-      if (length(bp) < 50) {
+      if (length(mp) < 50) {
         size.adj <- 0.75 
-      } else if (length(bp) < 80) {
+      } else if (length(mp) < 80) {
         size.adj <- 0.65  
-      } else if (length(bp) < 100) {
-        size.adj <- 0.4 # .5
-      } else if (length(bp) < 120) {
+      } else if (length(mp) < 100) {
+        size.adj <- 0.4 
+      } else if (length(mp) < 120) {
         size.adj <- 0.4
-      } else if (length(bp) < 150) {
+      } else if (length(mp) < 150) {
         size.adj <- 0.3
       } else {
         size.adj <- 0.3
       }
       cnames <- colnames(exposure)
       cnames <- sub("_____.*", "", cnames)
-      mtext(cnames, side = 1, line = 0.38, at = bp, las = 2, cex = size.adj)
+      mtext(cnames, side = 1, line = 0.38, at = mp, las = 2, cex = size.adj)
     }
     
-    invisible(list(plot.success = TRUE, plot.object = bp))
+    invisible(list(plot.success = TRUE, mp.coordinates = mp))
   }
 
 #' Plot exposures in multiple plots each with a manageable number of samples
 #'
-#' @param exposure Exposures as a numerical matrix (or data.frame) with
-#'   signatures in rows and samples in columns. Rownames are taken as the
-#'   signature names and column names are taken as the sample IDs. If you want
-#'   \code{exposure} sorted from largest to smallest, use
-#'   \code{\link{SortExposure}}. Do not use column names that start with
-#'   multiple underscores. The exposures will often be mutation counts, but
-#'   could also be e.g. mutations per megabase.
-#'
-#' @param plot.proportion Plot exposure proportions rather than counts.
+#' @inheritParams PlotExposureInternal
 #'
 #' @param samples.per.line Number of samples to show in each plot.
 #' 
-#' @param cex.legend A numerical value giving the amount by which legend
-#'   plotting text and symbols should be magnified relative to the default.
+#' @param xlim,ylim Limits for the x and y axis. If \code{NULL}(default), the
+#'   function tries to do something reasonable.
 #'
-#' @param ... Other arguments passed to \code{\link{PlotExposure}}. If
-#'   \code{ylab} is not included, it defaults to a value depending on
-#'   \code{plot.proportion}. If \code{col} is not supplied the function tries to
-#'   do something reasonable.
+#' @param legend.x,legend.y The x and y co-ordinates to be used to position the
+#'   legend.
+#'   
+#' @return An \strong{invisible} list whose first element is a logic value
+#'   indicating whether the plot is successful. The second element is a numeric
+#'   vector giving the coordinates of all the bar midpoints drawn, useful for
+#'   adding to the graph.
 #'
 #' @export
 #' 
@@ -266,18 +301,13 @@ PlotExposureInternal <-
 PlotExposure <- function(exposure,
                          samples.per.line = 30,
                          plot.proportion  = FALSE,
+                         xlim             = NULL,
+                         ylim             = NULL,
+                         legend.x         = NULL,
+                         legend.y         = NULL,
                          cex.legend       = 0.9,
                          ...
 ) {
-  new.xlim = c(0, samples.per.line * 1.25)
-  args <- list(...)
-  ylab <- args$ylab
-  if (is.null(ylab)) {
-    ylab <- ifelse(plot.proportion,
-                   "Proportion of mutations",
-                   "Number of mutations")
-  }
-  
   n.sample <- ncol(exposure)
   num.ranges <- n.sample %/% samples.per.line
   size.of.last.range <- n.sample %% samples.per.line
@@ -294,24 +324,49 @@ PlotExposure <- function(exposure,
   }
   ends   <- starts + samples.per.line - 1
   
-  plot.legend <- TRUE
   for (i in 1:length(starts)) {
-    PlotExposureInternal(exposure[ , starts[i]:ends[i]],
-                         plot.proportion = plot.proportion,
-                         plot.legend     = plot.legend,
-                         cex.legend      = cex.legend,
-                         ...             = ...)
-    #plot.legend <- FALSE
+    list <- PlotExposureInternal(exposure[ , starts[i]:ends[i]],
+                                 plot.proportion = plot.proportion,
+                                 xlim            = xlim,
+                                 ylim            = ylim,
+                                 legend.x        = legend.x,
+                                 legend.y        = legend.y,
+                                 cex.legend      = cex.legend,
+                                 ...             = ...)
   }
-  invisible(list(plot.success = TRUE))
+  invisible(list(plot.success = TRUE, mp.coordinates = list$mp.coordinates))
 }
 
 #' Plot exposures in multiple plots each with a manageable number of samples to PDF
 #' 
-#' @inheritParams PlotExposure
+#' @inheritParams PlotExposureInternal
 #' 
 #' @param file The name of the PDF file to be produced.
+#' 
+#' @param mfrow A vector of the form \code{c(nr, nc)}.
+#' Subsequent figures will be drawn in an \code{nr}-by-\code{nc}
+#' array on the device by rows.
+#' 
+#' @param mar A numerical vector of the form \code{c(bottom,
+#' left, top, right)} which gives the number of lines of margin to be
+#' specified on the four sides of the plot.
+#' 
+#' @param oma A vector of the form \code{c(bottom, left, top,
+#' right)} giving the size of the outer margins in lines of text.
+#' 
+#' @param samples.per.line Number of samples to show in each plot.
+#' 
+#' @param xlim,ylim Limits for the x and y axis. If \code{NULL}(default), the
+#'   function tries to do something reasonable.
 #'
+#' @param legend.x,legend.y The x and y co-ordinates to be used to position the
+#'   legend.
+#'   
+#' @return An \strong{invisible} list whose first element is a logic value
+#'   indicating whether the plot is successful. The second element is a numeric
+#'   vector giving the coordinates of all the bar midpoints drawn, useful for
+#'   adding to the graph.
+#' 
 #' @export
 #' 
 #' @examples 
@@ -322,23 +377,34 @@ PlotExposure <- function(exposure,
 #' PlotExposureToPdf(exposure, file = file.path(tempdir(), "exposure.pdf"))
 PlotExposureToPdf <- function(exposure,
                               file,  
+                              mfrow            = c(2, 1),
+                              mar              = c(2, 4, 3, 2),
+                              oma              = c(3, 2, 0, 2),
                               samples.per.line = 30,
                               plot.proportion  = FALSE,
+                              xlim             = NULL,
+                              ylim             = NULL,
+                              legend.x         = NULL,
+                              legend.y         = NULL,
                               cex.legend       = 0.9,
                               ...
 ) {
   # Setting the width and length for A4 size plotting
   grDevices::cairo_pdf(file, width = 8.2677, height = 11.6929, onefile = TRUE)
   
-  opar <- par(mfrow = c(2, 1), mar = c(2, 2, 3, 2), oma = c(3, 3, 0, 2))
+  opar <- par(mfrow = mfrow, mar = mar, oma = oma)
   on.exit(par(opar))
   
-  PlotExposure(exposure         = exposure, 
-               samples.per.line = samples.per.line,
-               plot.proportion  = plot.proportion, 
-               cex.legend       = cex.legend,
-               ...              = ...)
+  list <- PlotExposure(exposure         = exposure, 
+                       samples.per.line = samples.per.line,
+                       plot.proportion  = plot.proportion,
+                       xlim             = xlim,
+                       ylim             = ylim,
+                       legend.x         = legend.x,
+                       legend.y         = legend.y,
+                       cex.legend       = cex.legend,
+                       ...              = ...)
   
   grDevices::dev.off()
-  invisible(list(plot.success = TRUE))
+  invisible(list(plot.success = TRUE, mp.coordinates = list$mp.coordinates))
 }
