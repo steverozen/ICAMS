7979f53c45d38ada4f6b2ec424a861745e093b0c
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-08-10 06:31:44 +0800

    Added test code for function StrelkaSBSVCFFilesToCatalog() dealing with discarded variants and annotated vcfs

--- tests/testthat/test_discarded_variants.R
+++ tests/testthat/test_discarded_variants.R
@@ -1,4 +1,4 @@
-context("Test functions dealing with discarded variants")
+context("Test functions dealing with discarded variants and annotated vcfs")
 
 test_that("ReadAndSplitMutectVCFs", {
   files <- list.files(path = "testdata/Mutect-GRCh37", full.names = TRUE)
@@ -23,18 +23,18 @@ test_that("ReadAndSplitStrelkaSBSVCFs", {
   files <- list.files(path = "testdata/Strelka-SBS-GRCh37", full.names = TRUE)
   files1 <- files[1:2]
   list.of.vcfs1 <- ReadAndSplitStrelkaSBSVCFs(files1)
-  expect_null(list.of.vcfs1$other.subs)
-  expect_false(is.null(list.of.vcfs1$multiple.alt))
+  expect_null(list.of.vcfs1$ThreePlus)
+  expect_null(list.of.vcfs1$multiple.alt)
   expect_null(list.of.vcfs1$not.analyzed)
   
   files2 <- files
   list.of.vcfs2 <- ReadAndSplitStrelkaSBSVCFs(files2)
   list.of.vcfs3 <- 
     expect_warning(ReadAndSplitStrelkaSBSVCFs(files2,
-                                          suppress.discarded.variants.warnings = FALSE))
+                                              suppress.discarded.variants.warnings = FALSE))
   
+  expect_null(list.of.vcfs2$ThreePlus)
   expect_false(is.null(list.of.vcfs2$multiple.alt))
-  expect_false(is.null(list.of.vcfs2$other.subs))
   expect_false(is.null(list.of.vcfs2$not.analyzed))
 })
 
@@ -45,10 +45,21 @@ test_that("MutectVCFFilesToCatalog", {
   expect_false(is.null(catalogs$discarded.variants))
   expect_null(catalogs$annotated.vcfs)
   
-  file1 <- "testdata/Mutect.GRCh37.with.discarded.variants.vcf"
-  files1 <- c(files, file1)
-  catalogs1 <- MutectVCFFilesToCatalog(files, ref.genome = "hg19", 
-                                       region = "genome")
+  catalogs1 <- 
+    MutectVCFFilesToCatalog(files, ref.genome = "hg19", 
+                            region = "genome", return.annotated.vcfs = TRUE)
+  expect_false(is.null(catalogs1$annotated.vcfs))
+})
+
+test_that("StrelkaSBSVCFFilesToCatalog", {
+  files <- list.files(path = "testdata/Strelka-SBS-GRCh37", full.names = TRUE)
+  catalogs <- StrelkaSBSVCFFilesToCatalog(files, ref.genome = "hg19", 
+                                          region = "genome")
+  expect_false(is.null(catalogs$discarded.variants))
+  expect_null(catalogs$annotated.vcfs)
   
-  
+  catalogs1 <- 
+    StrelkaSBSVCFFilesToCatalog(files, ref.genome = "hg19", 
+                                region = "genome", return.annotated.vcfs = TRUE)
+  expect_false(is.null(catalogs1$annotated.vcfs))
 })
