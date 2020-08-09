7476ba20435861572b21cabf069269fbdc4726e3
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-08-09 16:46:34 +0800

    Updated test functions for discarded variants

--- tests/testthat/test_discarded_variants.R
+++ tests/testthat/test_discarded_variants.R
@@ -2,21 +2,21 @@ context("Test functions dealing with discarded variants")
 
 test_that("ReadAndSplitMutectVCFs", {
   files <- list.files(path = "testdata/Mutect-GRCh37/", full.names = TRUE)
-  list.of.vcfs <- ReadAndSplitMutectVCFs(files)
-  expect_null(list.of.vcfs$other.subs)
-  expect_false(is.null(list.of.vcfs$multiple.alt))
-  expect_null(list.of.vcfs$not.analyzed)
-  
-  file1 <- "testdata/Mutect.GRCh37.with.discarded.variants.vcf"
-  files1 <- c(files, file1)
+  files1 <- files[1:2]
   list.of.vcfs1 <- ReadAndSplitMutectVCFs(files1)
-  list.of.vcfs2 <- 
-    expect_warning(ReadAndSplitMutectVCFs(files1,
+  expect_null(list.of.vcfs1$other.subs)
+  expect_false(is.null(list.of.vcfs1$multiple.alt))
+  expect_null(list.of.vcfs1$not.analyzed)
+  
+  files2 <- files
+  list.of.vcfs2 <- ReadAndSplitMutectVCFs(files2)
+  list.of.vcfs3 <- 
+    expect_warning(ReadAndSplitMutectVCFs(files2,
                                           suppress.discarded.variants.warnings = FALSE))
     
-  expect_false(is.null(list.of.vcfs1$multiple.alt))
-  expect_false(is.null(list.of.vcfs1$other.subs))
-  expect_false(is.null(list.of.vcfs1$not.analyzed))
+  expect_false(is.null(list.of.vcfs2$multiple.alt))
+  expect_false(is.null(list.of.vcfs2$other.subs))
+  expect_false(is.null(list.of.vcfs2$not.analyzed))
 })
 
 test_that("MutectVCFFilesToCatalog", {
