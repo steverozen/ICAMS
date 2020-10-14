2bbd830bb23f227a1355d47f6d3717f485731309
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-10-14 21:02:45 +0800

    Create test_VCFsToCatalogAndPlotToPdf.R

---
+++ tests/testthat/test_VCFsToCatalogAndPlotToPdf.R
@@ -0,0 +1,43 @@
+context("VCFsToCatalogsAndPlotToPdf functions")
+
+test_that("CatalogAndPlotToPdf function for Mutect VCFs", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  files <- list.files(path = "testdata/Mutect-GRCh37", full.names = TRUE)
+  catalogs1 <- VCFsToCatalogsAndPlotToPdf(files, ref.genome = "hg19", 
+                                         output.dir = tempdir(),
+                                         variant.caller = "mutect",
+                                         region = "genome",
+                                         base.name = "mutect")
+  names <- grep("\\.pdf$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(names), 8)
+  unlink(file.path(tempdir(), names))
+})
+
+test_that("CatalogAndPlotToPdf function for Strelka SBS VCFs", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  files <- list.files(path = "testdata/Strelka-SBS-GRCh37/", full.names = TRUE)
+  catalogs1 <- VCFsToCatalogsAndPlotToPdf(files, ref.genome = "hg19", 
+                                         output.dir = tempdir(),
+                                         variant.caller = "strelka",
+                                         region = "genome",
+                                         base.name = "strelka.sbs")
+  names <- grep("\\.pdf$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(names), 8)
+  unlink(file.path(tempdir(), names))
+})
+
+test_that("CatalogAndPlotToPdf function for Strelka ID VCFs", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  files <- list.files(path = "testdata/Strelka-ID-GRCh37/", full.names = TRUE)
+  catalogs1 <- VCFsToCatalogsAndPlotToPdf(files, ref.genome = "hg19", 
+                                         output.dir = tempdir(),
+                                         variant.caller = "strelka",
+                                         region = "genome",
+                                         base.name = "strelka.id")
+  names <- grep("\\.pdf$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(names), 8)
+  unlink(file.path(tempdir(), names))
+})
