e9908622651ef5ab8258b51ebc33e98c0d9b1d6d
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-10-15 12:01:01 +0800

    Update test_1VCFFilesToZipFile.R

--- tests/testthat/test_1VCFFilesToZipFile.R
+++ tests/testthat/test_1VCFFilesToZipFile.R
@@ -150,3 +150,152 @@ test_that("StrelkaIDVCFFilesToZipFile function with base.filename", {
   graphics.off()
   unlink("testdata/Strelka-ID-GRCh37/Rplots.pdf")
 })
+
+test_that("VCFsToZipFile function for Mutect VCFs with no base.filename", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  dir <- "testdata/Mutect-GRCh37"
+  out <- VCFsToZipFile(dir, 
+                       zipfile = paste0(tempdir(), "/test.zip"), 
+                       ref.genome = "hg19", variant.caller = "mutect",
+                       region = "genome")
+  expect_type(out, "list")
+  name <- grep("\\.zip$", list.files(tempdir()), value = TRUE)
+  expect_equal(name, "test.zip")
+  zip::unzip(zipfile = paste0(tempdir(), "/test.zip"), exdir = tempdir())
+  name1 <- grep("\\.csv$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name1), 7)
+  name2 <- grep("\\.pdf$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name2), 8)
+  
+  unlink(file.path(tempdir(), "test.zip"))
+  unlink(file.path(tempdir(), name1))
+  unlink(file.path(tempdir(), name2))
+  graphics.off()
+  unlink("testdata/Mutect-GRCh37/Rplots.pdf")
+})
+
+test_that("VCFsToZipFile function for Mutect VCFs with base.filename", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  dir <- "testdata/Mutect-GRCh37"
+  out <- VCFsToZipFile(dir, 
+                       zipfile = paste0(tempdir(), "/test.zip"), 
+                       ref.genome = "hg19",
+                       region = "genome", variant.caller = "mutect",
+                       base.filename = "test")
+  expect_type(out, "list")
+  name <- grep("\\.zip$", list.files(tempdir()), value = TRUE)
+  expect_equal(name, "test.zip")
+  zip::unzip(zipfile = paste0(tempdir(), "/test.zip"), exdir = tempdir())
+  name1 <- grep("\\.csv$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name1), 7)
+  name2 <- grep("\\.pdf$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name2), 8)
+  
+  unlink(file.path(tempdir(), "test.zip"))
+  unlink(file.path(tempdir(), name1))
+  unlink(file.path(tempdir(), name2))
+  graphics.off()
+  unlink("testdata/Mutect-GRCh37/Rplots.pdf")
+})
+
+test_that("VCFsToZipFile function for Strelka SBS VCFs with no base.filename", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  dir <- "testdata/Strelka-SBS-GRCh37"
+  out <- VCFsToZipFile(dir,
+                       zipfile = paste0(tempdir(), "/test.zip"), 
+                       ref.genome = "hg19", variant.caller = "strelka",
+                       region = "genome")
+  expect_type(out, "list")
+  name <- grep("\\.zip$", list.files(tempdir()), value = TRUE)
+  expect_equal(name, "test.zip")
+  zip::unzip(zipfile = paste0(tempdir(), "/test.zip"), exdir = tempdir())
+  name1 <- grep("\\.csv$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name1), 7)
+  name2 <- grep("\\.pdf$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name2), 8)
+  
+  unlink(file.path(tempdir(), "test.zip"))
+  unlink(file.path(tempdir(), name1))
+  unlink(file.path(tempdir(), name2))
+  graphics.off()
+  unlink("testdata/Strelka-SBS-GRCh37/Rplots.pdf")
+})
+
+test_that("VCFsToZipFile function for Strelka SBS VCFs with base.filename", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  dir <- "testdata/Strelka-SBS-GRCh37"
+  out <- VCFsToZipFile(dir,
+                       zipfile = paste0(tempdir(), "/test.zip"), 
+                       ref.genome = "hg19",
+                       region = "genome", variant.caller = "strelka",
+                       base.filename = "test")
+  expect_type(out, "list")
+  name <- grep("\\.zip$", list.files(tempdir()), value = TRUE)
+  expect_equal(name, "test.zip")
+  zip::unzip(zipfile = paste0(tempdir(), "/test.zip"), exdir = tempdir())
+  name1 <- grep("\\.csv$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name1), 7)
+  name2 <- grep("\\.pdf$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name2), 8)
+  
+  unlink(file.path(tempdir(), "test.zip"))
+  unlink(file.path(tempdir(), name1))
+  unlink(file.path(tempdir(), name2))
+  graphics.off()
+  unlink("testdata/Strelka-SBS-GRCh37/Rplots.pdf")
+})
+
+test_that("VCFsToZipFile function for Strelka ID VCFs with no base.filename", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  dir <- "testdata/Strelka-ID-GRCh37"
+  out <- VCFsToZipFile(dir,
+                       zipfile = paste0(tempdir(), "/test.zip"),
+                       ref.genome = "hg19", variant.caller = "strelka",
+                       region = "genome")
+  expect_type(out, "list")
+  name <- grep("\\.zip$", list.files(tempdir()), value = TRUE)
+  expect_equal(name, "test.zip")
+  
+  zip::unzip(zipfile = paste0(tempdir(), "/test.zip"), exdir = tempdir())
+  name1 <- grep("\\.csv$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name1), 7)
+  name2 <- grep("\\.pdf$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name2), 8)
+  
+  unlink(file.path(tempdir(), "test.zip"))
+  unlink(file.path(tempdir(), name1))
+  unlink(file.path(tempdir(), name2))
+  graphics.off()
+  unlink("testdata/Strelka-ID-GRCh37/Rplots.pdf")
+})
+
+test_that("VCFsToZipFile function for Strelka ID VCFs with base.filename", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+  dir <- "testdata/Strelka-ID-GRCh37"
+  out <- VCFsToZipFile(dir,
+                       zipfile = paste0(tempdir(), "/test.zip"),
+                       ref.genome = "hg19", variant.caller = "strelka",
+                       region = "genome",
+                       base.filename = "test")
+  expect_type(out, "list")
+  name <- grep("\\.zip$", list.files(tempdir()), value = TRUE)
+  expect_equal(name, "test.zip")
+  
+  zip::unzip(zipfile = paste0(tempdir(), "/test.zip"), exdir = tempdir())
+  name1 <- grep("\\.csv$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name1), 7)
+  name2 <- grep("\\.pdf$", list.files(tempdir()), value = TRUE)
+  expect_equal(length(name2), 8)
+  
+  unlink(file.path(tempdir(), "test.zip"))
+  unlink(file.path(tempdir(), name1))
+  unlink(file.path(tempdir(), name2))
+  graphics.off()
+  unlink("testdata/Strelka-ID-GRCh37/Rplots.pdf")
+})
