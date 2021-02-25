638312f4d02c848b0fb2cb6d59ade2626921ef9a
Wu,Yang <wuyangff95@yahoo.com>
2021-02-25 12:36:39 +0800

    1. Added SigPro-formatted catalog and exposures. 2. Updated tests related to SigPro-formatted catalogs. 3. Reading SigPro-formatted catalog no longer throws warnings.

---
+++ tests/testthat/test_ReadCatalog.R
@@ -0,0 +1,42 @@
+test_that("Functions for converting COSMIC and SigProfiler-formatted catalogs to ICAMS", {
+  skip_if("" == system.file(package = "BSgenome.Hsapiens.1000genomes.hs37d5"))
+  stopifnot(requireNamespace("BSgenome.Hsapiens.1000genomes.hs37d5"))
+
+  my.file <-
+    c("testdata/COSMIC-Cat/COSMIC_SBS1_SBS96.csv",
+      "testdata/COSMIC-Cat/COSMIC_SBS1_TSB192.csv",
+      "testdata/COSMIC-Cat/COSMIC_DBS1_DBS78.csv",
+      "testdata/COSMIC-Cat/COSMIC_ID1_ID96.csv",
+      "testdata/SigPro-Cat/21BRCA.SBS96.tsv",
+      "testdata/SigPro-Cat/21BRCA.DBS78.tsv",
+      "testdata/SigPro-Cat/Strelka.ID.GRCh37.s1.ID83.tsv",
+      "testdata/SigPro-Cat/Strelka.ID.GRCh37.s1.ID96.tsv"
+    )
+  
+  my.region <-
+    c("genome",
+      "transcript",
+      "genome",
+      "genome",
+      "genome",
+      "genome",
+      "genome",
+      "genome"
+      )
+
+  Test1Cat <- function(my.file, my.region) {
+    ct1 <- ReadCatalog(file = my.file, ref.genome = "GRCh37",
+                       region = my.region, 
+                       catalog.type = "counts",
+                       stop.on.error = TRUE)
+    f <- tempfile("catalogConv")
+    WriteCatalog(ct1, f)
+    ct2 <- ReadCatalog(f, ref.genome = "GRCh37",
+                       region = my.region, catalog.type = "counts")
+    expect_equal(ct1, ct2)
+  }
+  
+  discard <- mapply(Test1Cat, my.file, my.region)
+  temp.files <- list.files(tempdir(), full.names = TRUE, pattern = "^catalogConv")
+  invisible(file.remove(temp.files)) 
+})
--- tests/testthat/test_TransRownames.ID.PCAWG.SigPro.R
+++ tests/testthat/test_TransRownames.ID.PCAWG.SigPro.R
@@ -3,8 +3,8 @@ test_that("Check sigprofiler-formatted indel catalog rownames",
           { 
             spIDCat <- read.csv("testdata/sigProfiler_ID_signatures.csv",
                             row.names = 1, 
-                            stringsAsFactors = F)
-            expect_equal(ICAMS::catalog.row.order.sp$ID83,rownames(spIDCat))
+                            stringsAsFactors = FALSE)
+            expect_equal(catalog.row.headers.sp$ID83,rownames(spIDCat))
             unlink("Rplots.pdf")
           })
 
@@ -14,13 +14,13 @@ test_that("TransRownames.ID.PCAWG.SigPro function",
           {
             inputRownames <- ICAMS::catalog.row.order$ID
             outputRownames <- TransRownames.ID.PCAWG.SigPro(inputRownames)
-            expect_true(setequal(outputRownames,ICAMS::catalog.row.order.sp$ID83))
+            expect_true(setequal(outputRownames,catalog.row.headers.sp$ID83))
           })
 
 
 test_that("TransRownames.ID.SigPro.PCAWG function", 
           {
-            inputRownames <- ICAMS::catalog.row.order.sp$ID83
+            inputRownames <- catalog.row.headers.sp$ID83
             outputRownames <- TransRownames.ID.SigPro.PCAWG(inputRownames)
             expect_true(setequal(outputRownames,ICAMS::catalog.row.order$ID))
           })
