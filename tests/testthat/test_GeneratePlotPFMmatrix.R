31dc71001e4c1be0691353fc91e8f12721b1bb4f
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-09-02 13:55:52 +0800

    Added plots for 1 bp DEL cases

--- tests/testthat/test_GeneratePlotPFMmatrix.R
+++ tests/testthat/test_GeneratePlotPFMmatrix.R
@@ -1,30 +1,47 @@
 test_that("GeneratePlotPFMmatrix", {
-
-  load("testdata/test_GeneratePlotPFMmatrix.Rdata")
-
-  p1 <- tempfile(fileext = "pdf")
+  # Please see "./data-raw/code/create_testdata_for_ID_extended_sequences.R"
+  # for generating the test data
   
-  test.INS.T.1.4.retval <- 
-    GeneratePlotPFMmatrix(sequences = INS.T.1.4.sequences,
-                          indel.class = "INS:T:1:4",
-                          plot.dir=p1,
-                          plot.title = "test.INS.T.1.4")
-
-  p2 <- tempfile(fileext = "pdf")
+  load("testdata/test_GeneratePlotPFMmatrix.Rdata")
+  
+  files <- file.path(tempdir(), paste0("test", 1:4, ".pdf"))
   
   test.INS.T.1.0.retval <- 
     GeneratePlotPFMmatrix(sequences = INS.T.1.0.sequences,
                           indel.class = "INS:T:1:0",
-                          plot.dir=p2,
+                          plot.dir = files[1],
                           plot.title = "test.INS.T.1.0")
+  
+  test.INS.T.1.4.retval <- 
+    GeneratePlotPFMmatrix(sequences = INS.T.1.4.sequences,
+                          indel.class = "INS:T:1:4",
+                          plot.dir = files[2],
+                          plot.title = "test.INS.T.1.4")
 
+  test.DEL.C.1.0.retval <- 
+    GeneratePlotPFMmatrix(sequences = DEL.C.1.0.sequences,
+                          indel.class = "DEL:C:1:0",
+                          plot.dir = files[3],
+                          plot.title = "test.DEL.C.1.0")
 
-  expect_equal(test.INS.T.1.4.retval, INS.T.1.4.retval)
+  
+  test.DEL.C.1.4.retval <- 
+    GeneratePlotPFMmatrix(sequences = DEL.C.1.4.sequences,
+                          indel.class = "DEL:C:1:4",
+                          plot.dir = files[4],
+                          plot.title = "test.DEL.C.1.4")
+  
   expect_equal(test.INS.T.1.0.retval, INS.T.1.0.retval)
-
+  expect_equal(test.INS.T.1.4.retval, INS.T.1.4.retval)
+  expect_equal(test.DEL.C.1.0.retval, DEL.C.1.0.retval)
+  expect_equal(test.DEL.C.1.4.retval, DEL.C.1.4.retval)
+  
+  sapply(files, FUN = unlink, recursive = TRUE)
 })
 
 test_that("errors", {
   expect_error(
-    test.INS.T.1.4.retval <- GeneratePlotPFMmatrix(sequences = c(INS.T.1.4.sequences,INS.T.1.0.sequences),indel.class = "INS:T:1:4"))
+    test.INS.T.1.4.retval <- 
+      GeneratePlotPFMmatrix(sequences = c(INS.T.1.4.sequences, INS.T.1.0.sequences),
+                            indel.class = "INS:T:1:4"))
 })
