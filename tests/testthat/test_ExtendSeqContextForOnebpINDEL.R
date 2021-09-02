1a3eda2802b2b6970afe38466527933fa6c65b70
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-09-02 13:55:34 +0800

    Reformatted code

--- tests/testthat/test_ExtendSeqContextForOnebpINDEL.R
+++ tests/testthat/test_ExtendSeqContextForOnebpINDEL.R
@@ -1,21 +1,18 @@
 
 test_that("ExtendSeqContextForOnebpINDEL", {
-
+  # Please see "./data-raw/code/create_testdata_for_ID_extended_sequences.R"
+  # for generating the test data
+  
   load("testdata/test_ExtendSeqContextForOnebpINDEL.Rdata")
 
-  #This is to generate test.ins.vcf from a vcf file, but this does not need to run every time
-  #test.ins.vcf <- ICAMS::ReadVCFs("testdata/Strelka-ID-GRCh37/Strelka.ID.GRCh37.s1.vcf")
-
-  #test.ins.vcf <- ICAMS::VCFsToIDCatalogs(list(test.ins.vcf$Strelka.ID.GRCh37.s1),ref.genome="hg19",region = "genome",return.annotated.vcfs = T)
-
-  #test.ins.vcf <- test.ins.vcf$annotated.vcfs[[1]]
-
-
-  test.INS.T.1.0.sequences <- SymmetricalContextsFor1BPIndel(test.ins.vcf,indel.class = "INS:T:1:0")
-  test.INS.T.1.4.sequences <- SymmetricalContextsFor1BPIndel(test.ins.vcf,indel.class = "INS:T:1:4")
-  test.DEL.C.1.0.sequences <- SymmetricalContextsFor1BPIndel(test.ins.vcf,indel.class = "DEL:C:1:0")
-  test.DEL.C.1.4.sequences <- SymmetricalContextsFor1BPIndel(test.ins.vcf,indel.class = "DEL:C:1:4")
-
+  test.INS.T.1.0.sequences <- 
+    SymmetricalContextsFor1BPIndel(test.indel.vcf, indel.class = "INS:T:1:0")
+  test.INS.T.1.4.sequences <- 
+    SymmetricalContextsFor1BPIndel(test.indel.vcf, indel.class = "INS:T:1:4")
+  test.DEL.C.1.0.sequences <- 
+    SymmetricalContextsFor1BPIndel(test.indel.vcf, indel.class = "DEL:C:1:0")
+  test.DEL.C.1.4.sequences <- 
+    SymmetricalContextsFor1BPIndel(test.indel.vcf, indel.class = "DEL:C:1:4")
 
   expect_equal(test.INS.T.1.0.sequences, INS.T.1.0.sequences)
   expect_equal(test.INS.T.1.4.sequences, INS.T.1.4.sequences)
@@ -25,12 +22,15 @@ test_that("ExtendSeqContextForOnebpINDEL", {
 
 test_that("errors", {
   expect_error(
-    Get1BPIndelFlanks("CTCTTTGTGAAGTAGCAACAGTTACATTTAAAATTTAAAACAC", "T",  "TT",  "INS:T:1:2"))
+    Get1BPIndelFlanks(sequence = "CTCTTTGTGAAGTAGCAACAGTTACATTTAAAATTTAAAACAC", 
+                      ref = "T",  alt = "TT",  indel.class = "INS:T:1:2"))
 # [1] "ACAGTTACATTT"
   expect_error(
-    Get1BPIndelFlanks("ATATAATTTGAATAAAAAGGTTTTGGAATACTGAAAAGTCTCC", "T",  "TT",  "INS:T:1:4"))
+    Get1BPIndelFlanks(sequence = "ATATAATTTGAATAAAAAGGTTTTGGAATACTGAAAAGTCTCC", 
+                      ref = "T",  alt = "TT",  indel.class = "INS:T:1:4"))
 # [1] "AGGTTTTGGAATAC"
   expect_error(
-    Get1BPIndelFlanks("CTGGCTAATTTTTTGTATTGTTTTTAGTAGAGACGGGGGTTTGC", "TT",  "T",  "DEL:T:1:4"))
+    Get1BPIndelFlanks(sequence = "CTGGCTAATTTTTTGTATTGTTTTTAGTAGAGACGGGGGTTTGC", 
+                      ref = "TT",  alt = "T",  indel.class = "DEL:T:1:4"))
 # [1] "TTGTTTTTAGTAGAG"
 })
