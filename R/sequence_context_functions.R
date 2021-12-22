b834b8920376f1da0a4af88e5cf619c127d13f75
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-12-22 16:06:23 +0800

    Documentation updates

--- R/sequence_context_functions.R
+++ R/sequence_context_functions.R
@@ -82,21 +82,6 @@ CreatePPMFromSBSVCFs <-
 #'
 #' @keywords internal
 #'
-#' @examples
-#' file <- c(system.file("extdata/Mutect-vcf",
-#'                       "Mutect.GRCh37.s1.vcf",
-#'                       package = "ICAMS"))
-#' split.vcfs <- ReadAndSplitVCFs(file, variant.caller = "mutect")
-#' ID.catalog <- VCFsToIDCatalogs(list.of.vcfs = split.vcfs$ID,
-#'                                ref.genome = "hg19",
-#'                                region = "genome",
-#'                                return.annotated.vcfs = TRUE)
-#' annotated.vcf <- ID.catalog$annotated.vcfs$Mutect.GRCh37.s1
-#' extended.seq.contexts <-
-#'   SymmetricalContextsFor1BPIndel(annotated.vcf = annotated.vcf,
-#'                                  indel.class = "DEL:T:1:0")
-
-#'
 SymmetricalContextsFor1BPIndel <-
   function(annotated.vcf, indel.class, flank.length = 5){
     
@@ -266,23 +251,6 @@ Get1BPIndelFlanks <- function(sequence, ref, alt, indel.class, flank.length = 5)
 #'
 #' @keywords internal
 #'
-#' @examples
-#' file <- c(system.file("extdata/Mutect-vcf",
-#'                       "Mutect.GRCh37.s1.vcf",
-#'                       package = "ICAMS"))
-#' split.vcfs <- ReadAndSplitVCFs(file, variant.caller = "mutect")
-#' ID.catalog <- VCFsToIDCatalogs(list.of.vcfs = split.vcfs$ID,
-#'                                ref.genome = "hg19",
-#'                                region = "genome",
-#'                                return.annotated.vcfs = TRUE)
-#' annotated.vcf <- ID.catalog$annotated.vcfs$Mutect.GRCh37.s1
-#' extended.seq.contexts <-
-#'   SymmetricalContextsFor1BPIndel(annotated.vcf = annotated.vcf,
-#'                                  indel.class = "DEL:T:1:0")
-#' GeneratePlotPFMmatrix(sequences = extended.seq.contexts,
-#'                       indel.class = "DEL:T:1:0",
-#'                       plot.dir = file.path(tempdir(), "test.pdf"),
-#'                       plot.title = "Deletion of 1T from 1T")
 GeneratePlotPFMmatrix <-
   function(sequences, indel.class, flank.length = 5, plot.dir = NULL,
            plot.title = NULL){
@@ -403,23 +371,6 @@ PlotPFMmatrix<-function(PFMmatrix, title,
 #' 
 #' @keywords internal
 #' 
-#' @examples
-#' file <- c(system.file("extdata/Mutect-vcf",
-#'                       "Mutect.GRCh37.s1.vcf",
-#'                       package = "ICAMS"))
-#' split.vcfs <- ReadAndSplitVCFs(file, variant.caller = "mutect")
-#' ID.catalog <- VCFsToIDCatalogs(list.of.vcfs = split.vcfs$ID,
-#'                                ref.genome = "hg19",
-#'                                region = "genome",
-#'                                return.annotated.vcfs = TRUE)
-#' annotated.vcf <- ID.catalog$annotated.vcfs$Mutect.GRCh37.s1
-#' extended.seq.contexts <-
-#'   SymmetricalContextsFor1BPIndel(annotated.vcf = annotated.vcf,
-#'                                  indel.class = "INS:T:1:4")
-#' ggplot.object <- HaplotypePlot(sequences = extended.seq.contexts,
-#'                                indel.class = "INS:T:1:4",
-#'                                title = "Deletion of 1T from 4Ts")
-#' plot(ggplot.object)                              
 HaplotypePlot <- function(sequences,
                           indel.class, flank.length = 5,
                           title="Haplotype Plot"){
