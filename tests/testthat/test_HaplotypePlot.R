612f1abce80194ffca0c3fe97de8288c25c8962b
Steven G. Rozen <steve@steverozen.net>
2024-09-02 21:13:08 -0400

    Steve is not sure if it makes sense to compare objects of class gg, ggplot. Possilby structure of the objects change when ggplot2 changes

--- tests/testthat/test_HaplotypePlot.R
+++ tests/testthat/test_HaplotypePlot.R
@@ -28,16 +28,20 @@ test_that("HaplotypePlot", {
                   indel.class = "DEL:C:1:4",
                   flank.length = 6,
                   title = "Deletion of 1C from 5Cs")
-
-  expect_equal(test.INS.T.1.0.retval, INS.T.1.0.retval)
-  expect_equal(test.INS.T.1.4.retval, INS.T.1.4.retval)
-  expect_equal(test.DEL.C.1.0.retval, DEL.C.1.0.retval)
-  expect_equal(test.DEL.C.1.4.retval, DEL.C.1.4.retval)
-
-  plot(test.INS.T.1.0.retval)
-  plot(test.INS.T.1.4.retval)
-  plot(test.DEL.C.1.0.retval)
-  plot(test.DEL.C.1.4.retval)
+  if (FALSE) {
+    # Steve is not sure if it makes sense to compare
+    # objects of class gg, ggplot. Possilby structure
+    # of the objects change when ggplot2 changes.
+    expect_equal(test.INS.T.1.0.retval, INS.T.1.0.retval)
+    expect_equal(test.INS.T.1.4.retval, INS.T.1.4.retval)
+    expect_equal(test.DEL.C.1.0.retval, DEL.C.1.0.retval)
+    expect_equal(test.DEL.C.1.4.retval, DEL.C.1.4.retval)
+  }
+  
+  expect_no_error(plot(test.INS.T.1.0.retval))
+  expect_no_error(plot(test.INS.T.1.4.retval))
+  expect_no_error(plot(test.DEL.C.1.0.retval))
+  expect_no_error(plot(test.DEL.C.1.4.retval))
 })
 
 test_that("errors", {
