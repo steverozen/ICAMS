fbea37f781c5c028d40ed450b13d7a06f86db1b9
Steven G. Rozen <steverozen+github@pm.me>
2025-10-28 13:39:42 -0400

    more tests

--- inst/testnewdelcode.R
+++ inst/testnewdelcode.R
@@ -7,20 +7,17 @@ vcf1 = ICAMS::ReadVCFs(f1, "mutect")[1]
 ivcf1 = dplyr::filter(vcf1[[1]], nchar(REF) != nchar(ALT))
 
 source("annotate_ids_in_vcf.R")
-source("xCanonicalize1ID.R")
-
 source("categorize_many_indels.R")
 source("gen_COSMIC_83_string.R")
 source("justify_indel.R")
-# debug(xCanonicalize1ID)
-# debug(annotate_ids_in_vcf)
-
-source("categorize_1_indel.R")
+source("xCanonicalize1ID.R")
+source("categorize_1_justified_indel.R")
 avcf1 = annotate_ids_in_vcf(
   ivcf1,
   "hg19",
   trans.ranges = NULL,
-  flag.mismatches = 0
+  flag.mismatches = 0,
+  explain_indels = TRUE
 )
 
 avcf1 = avcf1$annotated.vcf
@@ -32,11 +29,18 @@ files <- list.files(
   full.names = TRUE
 )
 catalogs1 <- MutectVCFFilesToCatalog(
-  #gardenview  Durham 2 7 7 1 3 862 360 0199
+  files,
+  ref.genome = "hg19",
+  region = "genome",
+  return.annotated.vcfs = TRUE
+)
 
+files <- list.files(
+  path = "c:/Users/steve/Documents/GitHub/ICAMS/tests/testthat/testdata/Strelka-ID-GRCh37/",
+  full.names = TRUE
+)
+catalogs1 <- StrelkaIDVCFFilesToCatalog(
+  files,
   ref.genome = "hg19",
   region = "genome"
 )
-
-# check function AnnotateIDVCF
-# debug(AnnotateIDVCF)
