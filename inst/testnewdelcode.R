bcc009d7b5196f87d38d282a893698d244524c71
Steven G. Rozen <steverozen+github@pm.me>
2025-10-28 00:10:02 -0400

    reorg for easier testing

--- inst/testnewdelcode.R
+++ inst/testnewdelcode.R
@@ -1,27 +1,21 @@
 library(ICAMS)
-source("new_id_fns.R")
-debug(xCanonicalize1ID)
+setwd("c:/Users/steve/Documents/GitHub/ICAMS/inst")
 
-files <- list.files(
-  path = "c:/Users/steve/Documents/GitHub/ICAMS/tests/testthat/testdata/Mutect-GRCh37",
-  full.names = TRUE
-)
-catalogs1 <- MutectVCFFilesToCatalog(
-  files,
-  ref.genome = "hg19",
-  region = "genome"
-)
-
-# check function AnnotateIDVCF
-# debug(AnnotateIDVCF)
 
 f1 = "c:/Users/steve/Documents/GitHub/ICAMS/tests/testthat/testdata/Mutect-GRCh37/Mutect.GRCh37.s1.vcf"
 vcf1 = ICAMS::ReadVCFs(f1, "mutect")[1]
 ivcf1 = dplyr::filter(vcf1[[1]], nchar(REF) != nchar(ALT))
 
-
 source("annotate_ids_in_vcf.R")
-debug(annotate_ids_in_vcf)
+source("xCanonicalize1ID.R")
+
+source("categorize_many_indels.R")
+source("gen_COSMIC_83_string.R")
+source("justify_indel.R")
+# debug(xCanonicalize1ID)
+# debug(annotate_ids_in_vcf)
+
+source("categorize_1_indel.R")
 avcf1 = annotate_ids_in_vcf(
   ivcf1,
   "hg19",
@@ -29,5 +23,20 @@ avcf1 = annotate_ids_in_vcf(
   flag.mismatches = 0
 )
 
-
 avcf1 = avcf1$annotated.vcf
+
+## Older, end-to-end tests
+
+files <- list.files(
+  path = "c:/Users/steve/Documents/GitHub/ICAMS/tests/testthat/testdata/Mutect-GRCh37",
+  full.names = TRUE
+)
+catalogs1 <- MutectVCFFilesToCatalog(
+  #gardenview  Durham 2 7 7 1 3 862 360 0199
+
+  ref.genome = "hg19",
+  region = "genome"
+)
+
+# check function AnnotateIDVCF
+# debug(AnnotateIDVCF)
