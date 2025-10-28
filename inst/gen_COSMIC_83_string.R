45b75da125b99d6e557c05d1abb53a46f026dcdc
Steven G. Rozen <steverozen+github@pm.me>
2025-10-28 13:39:03 -0400

    correction of major error

--- inst/gen_COSMIC_83_string.R
+++ inst/gen_COSMIC_83_string.R
@@ -7,28 +7,30 @@ gen_COSMIC_83_string = function(arglist) {
     INS_OR_DEL = "INS:"
     rep_count = arglist$rep_count
   }
+
   rep_count_string = ifelse(rep_count >= 5, "5+", rep_count)
   rm(rep_count)
   if (nchar(arglist$ins_or_del_seq) == 1) {
     return(paste0(INS_OR_DEL, arglist$ins_or_del_seq, ":1:", rep_count_string))
-  } else {
-    if (arglist$ins_or_del == "d") {
-      size_string = as.character(nchar(arglist$ins_or_del_seq))
-      if (size_string >= 5) {
-        size_string = "5+"
-      }
-      if (!is.na(arglist$mh)) {
-        mh_string = ifelse(arglist$mh >= 5, "5+", arglist$mh)
-        return(paste0("DEL:MH:", arglist$mh, size_string))
-      } else {
-        return(paste0(
-          INS_OR_DEL,
-          "repeats:",
-          size_string,
-          ":",
-          rep_count_string - 1
-        ))
-      }
-    }
   }
+
+  size_string = nchar(arglist$ins_or_del_seq)
+  if (size_string >= 5) {
+    size_string = "5+"
+  } else {
+    size_string = as.character(size_string)
+  }
+
+  if (arglist$ins_or_del == "d" && !is.na(arglist$mh)) {
+    mh_string = ifelse(arglist$mh >= 5, "5+", arglist$mh)
+    return(paste0("DEL:MH:", size_string, ":", mh_string))
+  }
+
+  return(paste0(
+    INS_OR_DEL,
+    "repeats:",
+    size_string,
+    ":",
+    rep_count_string
+  ))
 }
