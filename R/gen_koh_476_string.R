71522d2ee375bb1fb8406670a7f9ba2778da829d
Steven G. Rozen <steverozen+github@pm.me>
2025-11-02 17:37:51 -0500

    Still working on this

--- R/gen_koh_476_string.R
+++ R/gen_koh_476_string.R
@@ -1,28 +1,85 @@
-gen_Koh_476_string = function(arglist) {
+xgen_Koh_476_string = function(arglist) {
   if (arglist$ins_or_del == "d") {
     INS_OR_DEL = "Del"
-    rep_count = arglist$unmutated_rep_count - 1
+    rep_count = arglist$unmutated_rep_count
   } else {
     stopifnot(arglist$ins_or_del == "i")
     INS_OR_DEL = "Ins"
     rep_count = arglist$unmutated_rep_count
   }
 
-  ins_or_del_str = args$ins_or_del_str
+  ins_or_del_seq = arglist$ins_or_del_seq
+  ins_or_del_len = nchar(ins_or_del_seq)
 
-  if (nchar(ins_or_del_str) == 1) {
+  if (ins_or_del_len == 1) {
+    # Lines 4 through 183 (insertions) and 244 through 405 (deletions) of Koh et al. sup table 7
     repcountstr = ifelse(rep_count >= 9, "9+", as.character(rep_count))
     return(paste0(
       arglist$pre,
       "[",
       INS_OR_DEL,
       "(",
-      ins_or_del_str,
-      ")R]",
+      ins_or_del_seq,
+      "):R",
       repcountstr,
+      "]",
       arglist$post
     ))
   }
+
+  if (!is.na(arglist$mh)) {
+    if (INS_OR_DEL == "Ins") {
+      # Insertion with microhomology
+      # Lines 184 and 185
+      if (rep_count != 0) {
+        browser()
+      }
+
+      return(
+        paste0(
+          INS_OR_DEL,
+          ifelse(ins_or_del_seq <= 4, "(2,4):M", "(5,):M")
+        )
+      )
+    } else {
+      # Deletion with microhomology
+      # Lines 454 through 474
+      if (rep_count != 1) {
+        browser()
+      }
+
+      del_mh_str = ifelse(arglist$mh >= 6, "(6,)", arglist$mh)
+      del_len_str = ifelse(ins_or_del_len >= 7, "(7,)", ins_or_del_len)
+      return(paste0(INS_OR_DEL, del_len_str, ":M", del_mh_str))
+    }
+  }
+
+  # Remaining classes of insertions and deletions;
+  #
+  # For deletion, L is the total length of the (now deleted)
+  # sequence, U is the length of the repeat unit (and there might be
+  # multiple repeat units within the deleted seqeucne), and R is
+  # the numbuer of repeat units prior to the deletion.
+
+  # For insertions, L is the total length of the inserted sequence
+  # and R is the number of repeat units prior to the
+  # insertion. We don't use U in the description, and figure 2
+  # in Koh et al doesn't show an example of cases where the
+  # inerted sequence has multiple repeat units, but we
+  # assume that R is defiened analogously to the case for
+  # insertions, so R is the number of repeat units
+
+  # There is an edge case:
+  #
+  # |ACAC|ACGTG where the intital sequence is either an insertion or deletion.
+  #
+  # If this is a a deletion this could be classified as microhomology Del4:M2.
+  # But could also be classified as Del4:U2:R3 (Koh-476) or Del(2,8):U(1,2)R(2:4) (Koh-89)
+  #
+  # If this is an insertion it could be Ins4:U2:R1 or Ins(2,4):M.  Since
+  # all Ins4:U2:R1 could be considered Ins(2,4):M, we assume that
+  # Ins4:U2:R1 takes precedence, and the analgous reasoning applies to
+  # deletions.
 
   rep_count_string = ifelse(rep_count >= 5, "5+", rep_count)
   rm(rep_count)
